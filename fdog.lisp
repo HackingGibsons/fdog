540a12ce485362de11d98f2ea1bc1fbab83afdc4
Yaroslav Shirokov <slava@vitrue.com>
2011-04-07 18:11:47 -0400

    [master] DB connection scaffolds, and they work, too

--- fdog.lisp
+++ fdog.lisp
@@ -2,6 +2,16 @@
 
 (in-package #:fdog)
 
-;;; "fdog" goes here. Hacks and glory await!
+(defvar *local-mongrel2-database-pathname* 
+  (make-pathname :directory '(:relative "server")
+                 :name "config" :type "sqlite")
+  "Wild pathname used to find the local mongrel2 database")
 
+(defvar *local-mongrel2-database* 
+  (namestring (first (directory *local-mongrel2-database-pathname*)))
+  "Proper pathname to the mongrel2 database")
+
+(defvar *mongrel2-database* 
+  (clsql:connect `(,*local-mongrel2-database*) :database-type :sqlite3)
+  "Database connection to the mongrel2 database")
 

2b8eb5d986560bbf0924aec1a1930c6df089f927
Yaroslav Shirokov <slava@vitrue.com>
2011-04-08 18:25:19 -0400

    [master] Models now have sane connection management

--- models/package.lisp
+++ models/package.lisp
@@ -1,14 +1,42 @@
 (defpackage :fdog-models
   (:use :cl)
-  (:export :connect))
-
+  (:export :connected-p :connect :disconnect :reconnect))
 (in-package :fdog-models)
 
-(defun connect ()
-  (format t "Connecting."))
+;; Knobs
+(defvar *server-database* nil
+  "A handle to the database connection to the server.")
+
+
+;; Methods
+(defun connected-p ()
+  "Bool of the current connection state"
+  (and *server-database* (member *server-database* (clsql:connected-databases))))
+
+(defun connect (&optional db-path)
+  "Connect to a mongrel2 configuration database.
+
+If `db-path' is omitted a default is used composed of:
+  `fdog:*default-server-database-path*' `fdog:*default-server-path*' `fdog:*default-root-path*'
+Merged into a pathname."
+  (let* ((paths (list fdog:*default-server-database-path* fdog:*default-server-path* fdog:*default-root-path*))
+         (db-path (or db-path (reduce #'merge-pathnames paths))))
+    (unless (connected-p)
+      (setf *server-database* (clsql:connect `(,(namestring db-path)))))))
 
 (defun disconnect ()
-  (format t "Disconnecting."))
+  "Disconnect from any existing database connecting we have.
 
+Return t if an action took place, nil otherwise"
+  (when (connected-p)
+    (clsql:disconnect :database *server-database*))
+  (when *server-database*
+    (not (setf *server-database* nil))))
 
+(defun reconnect ()
+  "Disconnect and reconnect to the database
 
+Returns two values, the result of disconnecting and connecting"
+  (let ((name (and (connected-p) (clsql:database-name *server-database*))))
+    (values (disconnect)
+            (connect name))))

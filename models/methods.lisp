e92b92c19c822c6be1cac2335d1c88dced64bc66
Yaroslav Shirokov <slava@vitrue.com>
2011-04-19 16:08:57 -0400

    [m2sh-proc-control] Some docs and a generic to specialize for mongrel controls

--- models/methods.lisp
+++ models/methods.lisp
@@ -4,7 +4,12 @@
 (defgeneric mongrel2-server-running-p (server)
   (:documentation "Determine if a given server is running or not."))
 
+(defgeneric mongrel2-server-signal-server (server signal)
+  (:documentation ":start :stop :restart :reload :status a given server")
+
 (defmethod mongrel2-server-running-p ((server mongrel2-server))
+  "T or NIL on (Running or Not-Running given a mongrel2-server
+Returns true of it can find a pidfile, and a process is running there."
   (let* ((pidfile (merge-pathnames (mongrel2-server-pidfile server)
                                    (mongrel2-server-root server)))
          (pid (and (probe-file pidfile)
@@ -12,7 +17,10 @@
     (and pid
          (eq (kill pid 0) 0))))
 
+;;; Model API Wrappers
 (defmethod mongrel2-server-pidfile :around ((server mongrel2-server))
+  "Wrap the query for a servers pidfile field to return a string
+lisp more easily accepts as a relative path"
   (let ((result (call-next-method)))
     (if (eql (char result 0) #\/)
         (subseq result 1)

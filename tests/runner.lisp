8ee8bbfe1f2f4a181ca7c255a88d95b45e827c81
Sean Bryant <sean@vitrue.com>
2011-05-02 14:36:56 -0400

    [fiveam] you know what? eval-when isn't needed since the system never gets loaded if you're not using.

--- tests/runner.lisp
+++ tests/runner.lisp
@@ -1,9 +1,8 @@
 (in-package :fdog-tests)
 
-(eval-when (:compile-toplevel :load-toplevel :execute)
-  (defparameter *tests-to-run* '(fdog-models-tests))
-  
-  (defun run-fdog-tests ()
-    (mapc #'run! *tests-to-run*))
+(defparameter *tests-to-run* '(fdog-models-tests))
 
-  (run-fdog-tests))
+(defun run-fdog-tests ()
+  (mapc #'run! *tests-to-run*))
+
+(run-fdog-tests)

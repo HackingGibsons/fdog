73cca57554ad569210911cdce2f03efac7fbcc6e
Yaroslav Shirokov <slava@vitrue.com>
2011-05-03 12:15:40 -0400

    [m2sh-script-working] Might as well?

--- server/config.fd.lisp
+++ server/config.fd.lisp
@@ -3,6 +3,6 @@
 (using-configuration!
  (with-server ("control" :addr "localhost" :port 1337 :chroot "./")
    (with-host ("localhost")
-     (make-route "/" (make-dir "public/")))))
+     (make-route "/" (make-dir "./public/")))))
 
 (format t "Loaded!~%")

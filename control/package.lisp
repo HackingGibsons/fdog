923aea363e0bd404d54ce1086dcada6cd8c05951
Yaroslav Shirokov <slava@vitrue.com>
2011-05-04 18:09:38 -0400

    [handle-self] Proof of concept handler

--- control/package.lisp
+++ control/package.lisp
@@ -9,4 +9,14 @@
 (defparameter *m2-recv* "tcp://127.0.0.1:13372")
 
 (defun run (&rest args &key &allow-other-keys)
-  (log-for (info) "Booting control handler with: ~A" args))
+  (log-for (trace) "Booting control handler with: ~A" args)
+  (m2cl:with-handler (handler *ident* *m2-send* *m2-recv*)
+    (loop
+       (log-for (trace) "Waiting for request!")
+       (let ((request (m2cl:handler-receive handler)))
+         (log-for (trace) "Got request!")
+         (m2cl:handler-send-http handler "I WIN!" :request request)
+         (m2cl:handler-close handler :request request)
+         (log-for (trace) "Response sent.")))))
+
+

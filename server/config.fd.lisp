7b866426754b10be3eb3bff57bd1e01859b19983
Yaroslav Shirokov <slava@vitrue.com>
2011-05-04 15:55:55 -0400

    [handle-self] Add control handler.

--- server/config.fd.lisp
+++ server/config.fd.lisp
@@ -3,6 +3,9 @@
 (using-configuration!
  (with-server ("control" :addr "localhost" :port 1337 :chroot "./")
    (with-host ("localhost")
-     (make-route "/" (make-dir "./public/")))))
+     (make-route "/" (make-handler :send-spec "tcp://127.0.0.1:13375" ;; 5 => S => Send, Get it?
+                                   :send-ident "control-send-ident"
+                                   :recv-spec "tcp://127.0.0.1:13372")) ;; 2 => R => Recieve, right? ..right? :(
+     (make-route "/static/" (make-dir "./public/")))))
 
 (format t "Loaded!~%")

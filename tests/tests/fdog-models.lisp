ee8628e907d39eef92b4f929947ef6f44523a57b
Yaroslav Shirokov <slava@vitrue.com>
2011-05-10 16:50:30 -0400

    [up-tests] Building out test for test server correctness

--- tests/tests/fdog-models.lisp
+++ tests/tests/fdog-models.lisp
@@ -35,3 +35,11 @@
   (let ((server (clsql:select 'mongrel2-server :flatp t :refresh t)))
     (is-false (null server))
     (is (= (length server) 1) "We really only should have the one test server")))
+
+(test (test-server-correct-p :fixture m2/with-server
+                                      :depends-on can-find-test-server)
+  (is-false (null server) "We should have a server when we use the server fixture")
+  (is (string-equal (mongrel2-server-name server) "testing")
+      "I have reason to suspect you're using the wrong server.")
+  (is (ppcre:scan "^127\." (mongrel2-server-addr server))
+      "The test server should only listen on localhost"))

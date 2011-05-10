f4d1fab99f02a81203a2e265dee6f93549c374f3
Yaroslav Shirokov <slava@vitrue.com>
2011-05-10 13:49:57 -0400

    [up-tests] Modified def-fixture that deletes before defining

--- tests/helpers/utils.lisp
+++ tests/helpers/utils.lisp
@@ -1,5 +1,28 @@
 (in-package :fdog-tests)
 
+#+sbcl
+(defmacro def-fixture (name args &body body)
+  "Defines a fixture named NAME. A fixture is very much like a
+macro but is used only for simple templating. A fixture created
+with DEF-FIXTURE is a macro which can use the special macrolet
+ to specify where the body should go.
+
+See Also: WITH-FIXTURE
+
+
+*note* that this overrides the default 5am DEF-FIXTURE. 5am's
+DEF-FIXTURE does not remove the old instance of the fixture.
+
+Modified def-fixture from: http://www.cliki.net/FiveAM
+Should stop SBCL from complainig about fixture edits without
+restarting the entire VM, which becomes tiresome if you're
+developing fixtures.
+by: Nixeagle"
+  `(eval-when (:compile-toplevel :load-toplevel :execute)
+     (rem-fixture ',name)
+     (setf (get-fixture ',name) (cons ',args ',body))
+     ',name))
+
 (defmacro def-mixture (name arglist mixins &body body)
   "Make a fixture mixing in the fixture macros."
   (let* ((g!eval-body (gensym "eval-body"))

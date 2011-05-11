7ed18996cbe50434b96ce38c758c908606715bf3
Yaroslav Shirokov <slava@vitrue.com>
2011-05-11 16:17:41 -0400

    [running-mongrel-tests] Got tired of the unreachable code warning. So I'm hiding it in this branch with a bunch of unrelated commits.

--- tests/tests/main.lisp
+++ tests/tests/main.lisp
@@ -2,8 +2,7 @@
 (in-suite main)
 
 (test cthulhu-rlyeh
-  (is-true "Hello world"
-           "That is not dead which can eternal lie, and in strange eons even death may die."))
+  (is-true "That is not dead which can eternal lie, and in strange eons even death may die."))
 
 
 

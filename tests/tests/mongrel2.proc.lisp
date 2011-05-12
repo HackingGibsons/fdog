d4bbb9d255370fa3273851f8d3ee62af29b800dc
Yaroslav Shirokov <slava@vitrue.com>
2011-05-11 22:16:59 -0400

    [mongrel2-test-dir-and-loopback-request] Can make a request to myself by bootstrapping myself

--- tests/tests/mongrel2.proc.lisp
+++ tests/tests/mongrel2.proc.lisp
@@ -5,3 +5,12 @@
                                :depends-on test-server-correct)
   (is (mongrel2-server-running-p server)
       "You cannot have a running server when you ask for one. [You should be able to] :("))
+
+(test (can-reach-test-static-content-over-HTTP :fixture m2/with-running-server
+                                               :depends-on can-have-running-server)
+  (let* ((url (format nil "http://~A:~A/static/"
+                                               (mongrel2-server-default-host-name server)
+                                               (mongrel2-server-port server)))
+         (response (drakma:http-request url)))
+    (is (ppcre:scan "^FDOG/OK" response) ;; TODO: Maybe slurp this from disk yourself?
+        "Did not get the expected responce.")))

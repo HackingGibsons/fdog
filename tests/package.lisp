88128045c8f658a38d8f16268e0f6e3530283a73
Sean Bryant <sean@vitrue.com>
2011-05-12 12:26:22 -0400

    [test-verbosity] have 5am::%run show test is currently running.

--- tests/package.lisp
+++ tests/package.lisp
@@ -11,3 +11,8 @@
 (defvar +server-bind+ "127.0.0.1")
 (defvar +server-port+ 7357)
 (defvar +default-host+ "localhost")
+
+(defmethod 5am::%run :around (test-spec)
+  (log-for (dribble) "About to run test: ~S" test-spec)
+  (let ((result (call-next-method)))
+    (log-for (dribble) "Finished running test: ~S" test-spec)))

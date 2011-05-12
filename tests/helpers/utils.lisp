b124332caaf8f5215513f0ce0db24cc43c98c7a0
Yaroslav Shirokov <slava@vitrue.com>
2011-05-12 01:34:36 -0400

    [master] Slight wrapper over drakma:http-request

--- tests/helpers/utils.lisp
+++ tests/helpers/utils.lisp
@@ -32,3 +32,20 @@ by: Nixeagle"
       `(def-fixture ,name ,arglist
          (macrolet ((,g!eval-body () '(progn ,@body)))
            ,callchain))))
+
+(defun http->string (url &rest args)
+  "A slightly repackaged veresion of drakma's client.
+Mostly exists to close the stream when required and repackage the 7-value
+return as something more edible (plist). Though discarding some values that
+are less usefull to string construction."
+  (multiple-value-bind
+        (response status-code headers uri stream must-close reason-phrase)
+        (apply 'drakma:http-request `(,url ,@args))
+
+    (when must-close (close stream))
+
+    (values response
+            (list :status-code status-code
+                  :headers     headers
+                  :uri         uri
+                  :reason      reason-phrase))))

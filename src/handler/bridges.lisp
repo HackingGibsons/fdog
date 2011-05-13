d8a13bbad9971b5057640e2a2e98881f958d464c
Yaroslav Shirokov <slava@vitrue.com>
2011-05-13 18:20:58 -0400

    [handlerize] Given a mongrel2-handler I can construct and fire up a request-handler endpoint

--- src/handler/bridges.lisp
+++ src/handler/bridges.lisp
@@ -25,4 +25,6 @@ given in `server'."
 (defmethod configure-bridges-for ((handler mongrel2-handler))
   "Configures a bridge in accordance with the handler passed.
 Returns just the matching request-handler instance."
-  :undef)
+  (make-instance 'request-handler :ident (mongrel2-handler-send-ident handler)
+                 :sub (mongrel2-handler-send-spec handler)
+                 :pub (mongrel2-handler-recv-spec handler)))

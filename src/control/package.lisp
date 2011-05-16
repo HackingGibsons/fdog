ac87b87f601e1dcdab042e46289d751a24973cd4
Yaroslav Shirokov <slava@vitrue.com>
2011-05-16 03:04:44 -0400

    [ui-endpoint] Export the *control-interface* symbol

--- src/control/package.lisp
+++ src/control/package.lisp
@@ -2,48 +2,21 @@
   (:use :sb-mop)
   (:use :cl :fdog-models :bordeaux-threads :fdog-handler)
   (:shadowing-import-from :log5 :log-for)
-  (:export :run))
+  (:export :*control-interface*
+           :fdog-interface
+           :fdog-interface-server
+           :fdog-interface-routes))
 (in-package :fdog-control)
 
-(defvar *interface-bridges* ()
-  "Bridges configured as part of the web UI")
+(defparameter *control-interface* nil
+  "The interface for interacting with FDOG externally")
 
-(defclass fdog-interface ()
-  ((server :initarg :server
-           :initform "control"
-           :accessor fdog-interface-server)
-   (bridges :initform ()
-            :accessor fdog-interface-bridges)
-   (routes :initform ()
-           :accessor fdog-interface-routes))
-  (:documentation "An interface for interacting with fdog through Mongrel2"))
-
-(defmethod initialize-instance :after ((self fdog-interface) &rest initargs)
-  (declare (ignore initargs))
-  (with-slots (server) self
-    (unless (typep server 'mongrel2-server)
-      (let ((servers (fdog-m2sh:servers :name server :refresh t)))
-        (if servers
-            (setf server (car servers))
-            (error (format nil "Could not initialize server from value: ~A" server))))))
-  (initialize-interface self))
-
-(defmethod initialize-interface ((self fdog-interface))
-  (with-slots (server bridges routes) self
-    (mapcar #'request-handler-stop bridges)
-    (setf bridges ()
-          routes ())
-
-    (dolist (route (mongrel2-host-routes (mongrel2-server-default-host server)) self)
-      (pushnew route routes)
-      (with-accessors ((target mongrel2-route-target)) route
-        (typecase target
-          (mongrel2-handler
-           (pushnew (configure-bridges-for target) bridges))
-          (otherwise
-           (unless target
-             (log-for (warn) "NIL TARGET FOR ROUTE ~A(~A)" route (slot-value route 'fdog-models::id)))))))))
-
+(defmethod init-control-interface (&key (server "control"))
+  "Creates a new control interface, killing the old, and starts it."
+  (when *control-interface*
+    (interface-stop *control-interface*))
+  (let ((interface (setf *control-interface* (make-instance 'fdog-interface :server server))))
+    (interface-start-server interface)))
 
 ;;; Scaffold
 ;; Some varsdefs

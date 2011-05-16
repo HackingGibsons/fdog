d1c0c83a96b2f147ce3b12745ecbdf8c4b33a75c
Yaroslav Shirokov <slava@vitrue.com>
2011-05-15 22:40:41 -0400

    [ui-endpoint] Exporting construction material

--- src/handler/package.lisp
+++ src/handler/package.lisp
@@ -14,11 +14,17 @@
              :request-handler-stop
              :request-handler-running-p
              ;; Responder chain builders
-             :request-handler-add-string-responder
-             :request-handler-add-chunked/start
-             :request-handler-add-chunked/chunk
-             :request-handler-add-chunked/stop
-             :request-handler-add-chunked/trailer
+             :request-handler-add-responder
+               :make-request-handler-string-responder
+                 :request-handler-add-string-responder
+               :request-handler-make-chunked-responder/start
+               :request-handler-make-chunked-responder/chunk
+               :request-handler-make-chunked-responder/stop
+               :request-handler-make-chunked-responder/trailer
+                 :request-handler-add-chunked/start
+                 :request-handler-add-chunked/chunk
+                 :request-handler-add-chunked/stop
+                 :request-handler-add-chunked/trailer
              ;; Bridge builders
              :configure-bridges-for))
 (in-package :fdog-handler)

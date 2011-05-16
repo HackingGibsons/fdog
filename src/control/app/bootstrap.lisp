d1ad607346212a4ce96ecac4b3238b3d864ebdc5
Yaroslav Shirokov <slava@vitrue.com>
2011-05-16 16:11:43 -0400

    [control-app] Proper dispatching!

--- src/control/app/bootstrap.lisp
+++ src/control/app/bootstrap.lisp
@@ -1,32 +1,12 @@
 (in-package :fdog-control)
 
-(defparameter *control-interface* nil
+(defvar *control-interface* nil
   "The interface for interacting with FDOG externally")
 
 (defun mount-control-application (bridge)
-  (log-for (trace) "Mounting control application bridge: ~A" bridge)
-  (flet ((response (r)
-           (format nil "~A:~A" (get-universal-time) (current-thread)))
-
-         (chunk-info/start (request)
-           (declare (ignorable request))
-           '((:X-hello-world . "I am awesome")
-             ("Trailer" . "X-Magic")))
-
-         (chunk-trailer (request)
-           `(("X-Magic" . "Totally")))
-
-         (chunk-two (request)
-           (declare (ignorable request))
-           (values "  Verily, this is the second reply!"
-                   (not (= (random 10) 0)))))
-    (request-handler-add-chunked/trailer bridge #'chunk-trailer)
-    (request-handler-add-chunked/stop bridge)
-
-    (request-handler-add-chunked/chunk bridge #'chunk-two)
-    (request-handler-add-chunked/chunk bridge #'response)
-
-    (request-handler-add-chunked/start bridge #'chunk-info/start)))
+  (log-for (trace) "Mounting control application on bridge: ~A" bridge)
+  (request-handler-add-responder bridge 'root/router :router)
+  (log-for (trace) "Mounted dispatcher on bridge."))
 
 (defmethod configure-control-routes (&optional (interface *control-interface*))
   (interface-configure-bridges (interface)
--- src/control/app/router.lisp
+++ src/control/app/router.lisp
@@ -1,2 +1,12 @@
 (in-package :fdog-control)
 
+(defun root/router (handler request raw)
+  (flet ((string-responder (req)
+           (format nil "Welcome to: ~A" (m2cl:request-path req))))
+    (let ((full-responder (make-request-handler-string-responder handler #'string-responder))
+          (path (m2cl:request-path request)))
+
+      (funcall full-responder handler request raw)
+
+      (log-for (trace) "Attempting to route for ~A" path))))
+

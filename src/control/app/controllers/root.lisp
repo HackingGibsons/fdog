3271cac77cb3f30b0321489c7411ad6723f1c968
Yaroslav Shirokov <slava@vitrue.com>
2011-05-17 18:18:28 -0400

    [control-app] Nicer name for chunked call chain construction

--- src/control/app/controllers/root.lisp
+++ src/control/app/controllers/root.lisp
@@ -10,7 +10,7 @@
 
       (log-for (trace) "Attempting to route for ~A" path))))
 
-(defmacro with-handler-chunked-reply-chain ((handler &key (code 200) (status "OK") (headers nil)) &body body)
+(defmacro with-chunked-reply-chain ((handler &key (code 200) (status "OK") (headers nil)) &body body)
   (let ((g!handler (gensym "handler"))
         (g!header-fun (gensym "header-fun"))
         (g!code (gensym "code"))
@@ -31,5 +31,5 @@
            (format nil "~A Not Found" (m2cl:request-path req))))
 
     (request-handler-respond-with-chain handler request raw
-      (with-handler-chunked-reply-chain (handler :code 404 :status "NOT FOUND")
+      (with-chunked-reply-chain (handler :code 404 :status "NOT FOUND")
         (request-handler-make-chunked-responder/chunk handler #'404-response)))))

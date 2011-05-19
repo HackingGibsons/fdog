29544f1192d20fc55b15226de265d673ae80444f
Yaroslav Shirokov <slava@vitrue.com>
2011-05-19 16:48:18 -0400

    [streaming-chunks] Stream encoder

---
+++ src/handler/streams.lisp
@@ -0,0 +1,23 @@
+(in-package :fdog-handler)
+
+(defclass chunked-http-output-stream (fundamental-output-stream fundamental-character-stream)
+  ((handler
+    :initarg :handler
+    :accessor chunked-stream-handler
+    :documentation "The m2cl handler instance to use for writing")
+   (request
+    :initarg :request
+    :accessor chanked-stream-request))
+  (:documentation "A stream for providing chunked HTTP output"))
+
+(defmethod stream-write-char ((stream chunked-http-output-stream) char)
+  (stream-write-sequence stream
+   (with-output-to-string (cheating) (format cheating "~C" char))))
+
+(defmethod stream-write-string ((stream chunked-http-output-stream) str &optional start end)
+  (stream-write-sequence stream (subseq str start end)))
+
+(defmethod stream-write-sequence ((stream chunked-http-output-stream) seq &optional start end)
+  (with-slots (handler request) stream
+    (m2cl:handler-send-http-chunk handler (subseq seq (or start 0) end) :request request)))
+

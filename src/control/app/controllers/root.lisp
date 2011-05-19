0d9f72f4f224eb7c27edba10554cf2462f7ef498
Yaroslav Shirokov <slava@vitrue.com>
2011-05-19 00:58:00 -0400

    [control-app] Shortened the / responder

--- src/control/app/controllers/root.lisp
+++ src/control/app/controllers/root.lisp
@@ -1,14 +1,9 @@
 (in-package :fdog-control)
 
 (defun root/respond (handler request raw)
-  (flet ((string-responder (req)
-           (format nil "Welcome to: ~A" (m2cl:request-path req))))
-    (let ((full-responder (make-request-handler-string-responder handler #'string-responder))
-          (path (m2cl:request-path request)))
+  (with-chunked-reply-chain-response (handler request raw)
+    (&chunk (format nil "Path: ~A~%" (m2cl:request-path request)))))
 
-      (funcall full-responder handler request raw)
-
-      (log-for (trace) "Attempting to route for ~A" path))))
 
 (defun root/404 (handler request raw)
   (with-chunked-reply-chain-response (handler request raw

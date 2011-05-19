b837ed1b0e37f8a68cae7e68775a55fc027c39dd
Yaroslav Shirokov <slava@vitrue.com>
2011-05-19 00:42:40 -0400

    [control-app] Noise reduction

--- src/control/app/controllers/root.lisp
+++ src/control/app/controllers/root.lisp
@@ -11,8 +11,6 @@
       (log-for (trace) "Attempting to route for ~A" path))))
 
 (defun root/404 (handler request raw)
-  (flet ((the-404 (r) (declare (ignore r)) "I am so fucking awesome"))
-    (with-chunked-reply-chain-response (handler request raw
-                                                :code 404 :status "NOT FOUND")
-      (&chunk #'the-404)
-      (&chunk (format nil "~%~A Not Found~%" (m2cl:request-path request))))))
+  (with-chunked-reply-chain-response (handler request raw
+                                              :code 404 :status "NOT FOUND")
+      (&chunk (format nil "~%~A Not Found~%" (m2cl:request-path request)))))

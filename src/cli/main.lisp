1014e6cf4cd26c6390067c388000cd88566e7390
Yaroslav Shirokov <slava@vitrue.com>
2011-05-24 16:13:19 -0400

    [cli] Quit after helping on command and clean up some args handing in the main entry point

--- src/cli/main.lisp
+++ src/cli/main.lisp
@@ -6,10 +6,8 @@
   (declare (ignorable argv)))
 
 
-(defcommand help (self &optional argv)
+(defcommand help (self &key (exit 0) command)
   "Show help"
-  (declare (ignorable argv))
-
   (format t "Usage: ~A <command> [command-options]~%" self)
   (format t "Available commands:~%")
   (dolist (command *commands*)
@@ -17,10 +15,18 @@
            (cmd-doc (get-command cmd-name :doc)))
       (format t "=> ~A~%" (string-downcase (symbol-name cmd-name)))
       (when cmd-doc
-        (format t "  ~A~%" cmd-doc)))))
+        (format t "  ~A~%" cmd-doc))))
+
+  (when exit (quit :unix-status 0)))
 
 ;; Commands and entries
 (defun fdog-main (argv)
+  ;; Don't want a CLI app dropping to a debugger repl
+  (sb-ext:disable-debugger)
+
   (destructuring-bind (self &rest args) argv
     (unless args
+      (funcall (get-command :help :function) self))
+    (unless (get-command (first args))
+      (format t "~A is not a valid command.~%" (first args))
       (funcall (get-command :help :function) self))))
--- src/cli/package.lisp
+++ src/cli/package.lisp
@@ -1,6 +1,8 @@
 (defpackage #:fdog-cli
   (:use #:cl)
   (:export :fdog-main)
+  (:shadowing-import-from :sb-ext
+                          :quit)
   (:shadowing-import-from :log5
                           :log-for :info :warn :error))
 (in-package :fdog-cli)

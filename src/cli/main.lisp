4d66f2c4709a87382ca02abad71bf663308d2b12
Yaroslav Shirokov <slava@vitrue.com>
2011-05-24 16:02:36 -0400

    [cli] Stop random noise, call for help if we have no args

--- src/cli/main.lisp
+++ src/cli/main.lisp
@@ -1,13 +1,26 @@
 (in-package :fdog-cli)
 
 ;; Commands
-(defcommand help (&optional argv)
-  (format t "Help~%"))
+(defcommand init (argv)
+  "Initialize an installation given by a path."
+  (declare (ignorable argv)))
+
+
+(defcommand help (self &optional argv)
+  "Show help"
+  (declare (ignorable argv))
+
+  (format t "Usage: ~A <command> [command-options]~%" self)
+  (format t "Available commands:~%")
+  (dolist (command *commands*)
+    (let* ((cmd-name (car command))
+           (cmd-doc (get-command cmd-name :doc)))
+      (format t "=> ~A~%" (string-downcase (symbol-name cmd-name)))
+      (when cmd-doc
+        (format t "  ~A~%" cmd-doc)))))
 
 ;; Commands and entries
 (defun fdog-main (argv)
   (destructuring-bind (self &rest args) argv
-    (format t "Main method: ~A :: ~A~%" self args)
-    (format t "Commands: ~A~%" *commands*)
     (unless args
-      (funcall (get-command :help :function)))))
+      (funcall (get-command :help :function) self))))

3018a2f5ea819ae79986a5f483926c6fa09e20d0
Yaroslav Shirokov <slava@vitrue.com>
2011-05-26 09:09:08 -0400

    [forwarder] +TODO note for futurerefactor

--- src/services/package.lisp
+++ src/services/package.lisp
@@ -3,6 +3,8 @@
   (:export :init-services))
 (in-package :fdog-services)
 
+;; TODO: Services could specialize :afters on this instead of me
+;;       piling up a bunch of services here
 (defmethod init-services (&rest services)
   (fdog-control:init-control-interface)
   (fdog-forwarder:init-forwarders))

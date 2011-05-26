47ef10c9c44499e8a9cf528c99969176bd5ce1e4
Yaroslav Shirokov <slava@vitrue.com>
2011-05-26 09:25:07 -0400

    [forwarder] Creating forwarder table if it does not exist

--- src/services/forwarder/package.lisp
+++ src/services/forwarder/package.lisp
@@ -3,17 +3,29 @@
   (:shadowing-import-from :fdog-control
                           :api/endpoint)
   (:shadowing-import-from :log5 :log-for)
-  (:shadowing-import-from :clsql)
 
   (:shadowing-import-from :fdog-control
                           :fdog-interface)
 
   (:export :init-forwarders))
-
 (in-package :fdog-forwarder)
 
 (defvar *forwarders* ()
   "List of the loaded forwarders")
+
+(clsql:def-view-class fdog-forwarder ()
+  ((id :type integer
+       :db-constraints (:primary-key :auto-increment)
+       :db-kind :key
+       :reader fdog-forwarder-id)
+   (host :type string
+         :initform nil)
+   (path :type string
+         :initform "/")
+   (forward-to :type string
+               :initform "tcp://localhost:9999"))
+  (:base-table fdog-forwarder
+   :documentation "Database model describing a forwarder endpoint."))
 
 (defclass fdog-forwarding-interface (fdog-interface)
   ((upstream :initarg :upstream
@@ -23,4 +35,7 @@
 (defmethod init-forwarders ()
   "Search for, init and start all known forwarders"
   (log-for (dribble) "Initializing forwarders..")
+  (when (not (clsql:table-exists-p (clsql:view-table (find-class 'fdog-forwarder))))
+    (log-for (trace) "Forwarder table does not exist.. creating.")
+    (clsql:create-view-from-class 'fdog-forwarder))
   :undef)

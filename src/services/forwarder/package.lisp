7652f3d143b74c823a6618e18b7364b01f454c2f
Yaroslav Shirokov <slava@vitrue.com>
2011-05-31 14:03:01 -0400

    [forwarder] Going to permanently bind a socket to write responses

--- src/services/forwarder/package.lisp
+++ src/services/forwarder/package.lisp
@@ -41,6 +41,9 @@
    (context :initform nil
             :initarg :context
             :accessor forwarder-context)
+   (response-write-sock :initform nil
+                        :initarg :response-write-sock
+                        :accessor forwarder-response-write-sock)
    (response-sock :initform nil
                 :initarg :listen-sock
                 :accessor forwarder-listen-sock)

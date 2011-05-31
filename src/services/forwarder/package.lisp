3eaef18201201357972fea9c796fcd5fd61fbc81
Yaroslav Shirokov <slava@vitrue.com>
2011-05-31 13:41:30 -0400

    [forwarder] Keeping context in the forwarder, and rename to not sound so dumb on the accessors

--- src/services/forwarder/package.lisp
+++ src/services/forwarder/package.lisp
@@ -36,15 +36,20 @@
 (defclass fdog-forwarding-interface (fdog-interface)
   ((upstream :initarg :upstream
              :accessor forwarder-upstream)
+   (context :initform nil
+            :initarg :context
+            :accessor forwarder-context)
    (response-sock :initform nil
                 :initarg :listen-sock
-                :accessor forwarding-interface-listen-sock)
+                :accessor forwarder-listen-sock)
    (request-sock :initform nil
                  :initarg :request-sock
-                 :accessor forwarding-interface-request-sock)
+                 :accessor forwarder-request-sock)
    (response-writer :initform nil
                     :accessor forwarding-interface-response-writer))
   (:documentation "An interface for forwarding requests to upstream 0mq endpoints."))
+
+
 
 (defmethod initialize-instance :after ((self fdog-forwarding-interface) &rest initargs)
   "Initialize the forwarder"

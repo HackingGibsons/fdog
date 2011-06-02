83865af27ec3830cac101fafa5d2a774718a0bda
Yaroslav Shirokov <slava@vitrue.com>
2011-06-02 00:05:33 -0400

    [forwarder] Wrapped the logger function

--- src/package.lisp
+++ src/package.lisp
@@ -24,10 +24,12 @@
                                (decode-universal-time (get-universal-time))
                              (format nil "[~D-~2,'0D-~2,'0D ~2,'0D:~2,'0D:~2,'0D]" year month date hour minute second)))
 
-(log5:start-sender 'default
-                   (log5:stream-sender :location *error-output*)
-                   :category-spec '(log5:dribble+)
-                   :output-spec '(human-time log5:category log5:message))
+(defun start-default-logging ()
+  (log5:start-sender 'default
+                     (log5:stream-sender :location *error-output*)
+                     :category-spec '(log5:dribble+)
+                     :output-spec '(human-time log5:category log5:message)))
+(start-default-logging)
 
 
 ;; Parameters of project-wide relevance

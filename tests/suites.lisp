700b4666cfebb35f4acc0db351c5d6934d563cd8
Yaroslav Shirokov <slava@vitrue.com>
2011-06-06 13:21:09 -0400

    [with-nst] Scaffolded more fixtures

--- tests/suites.lisp
+++ tests/suites.lisp
@@ -23,15 +23,30 @@
      :cleanup (log-for (trace) "DB Disconnected"))
   (db-path "something"))
 
+(def-fixtures database/configured
+    (:setup (log-for (trace) "DB configured setup")
+     :cleanup (log-for (trace) "DB configured cleanup"))
+  (db-init "something-else"))
+
+(def-fixtures mongrel2/running
+    (:setup (log-for (trace) "Mongrel2 setup")
+     :cleanup (log-for (trace) "Mongrel2 cleanup"))
+  (mongrel2 "something-else-entirely"))
+
+
+
 (def-test-group database-basic-tests (database/connected)
-  (:documentation "'Database baisc tests")
+  (:documentation "Database baisc tests"))
 
-  (def-test test-database-sanity :pass)
+(def-test-group mongrel2-database-tests (database/connected database/configured)
+  (:documentation "Tests of the mongrel2 database access"))
 
-  (def-eval-test test-database-insanity
-      (assert-null nil)))
+(def-test-group mongrel2-process-tests (database/connected database/configured mongrel2/running)
+  (:documentation "Tests including a running mongrel2."))
 
 (def-test-group main-tests ()
   (:documentation "'The' test group.")
-  (:include-groups database-basic-tests))
+  (:include-groups database-basic-tests
+                   mongrel2-database-tests
+                   mongrel2-process-tests))
 

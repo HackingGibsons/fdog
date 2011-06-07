b2ff7518acc617f2a5bd3767c5fce375e476f124
Yaroslav Shirokov <slava@vitrue.com>
2011-06-06 23:24:03 -0400

    [with-nst] Full test port over to NST with junit out

--- tests/tests/mongrel2.proc.lisp
+++ tests/tests/mongrel2.proc.lisp
@@ -10,12 +10,10 @@
 (def-test+m2/running can-have-running-server
   :true (mongrel2-server-running-p server))
 
-;; (test (can-reach-test-static-content-over-HTTP :fixture m2/with-running-server
-;;                                                :depends-on can-have-running-server)
-;;   (let* ((url (format nil "http://~A:~A/static/"
-;;                       (mongrel2-server-default-host-name server)
-;;                       (mongrel2-server-port server)))
-;;          (response (or (http->string url) "")))
-
-;;     (is (ppcre:scan "^FDOG/OK" response) ;; TODO: Maybe slurp this from disk yourself?
-;;           "Did not get the expected responce.")))
+(def-test+m2/running can-reach-test-statuc-content-over-HTTP
+  :eval
+  (let* ((url (format nil "http://~A:~A/static/"
+                      (mongrel2-server-default-host-name server)
+                      (mongrel2-server-port server)))
+         (response (or (http->string url) "")))
+    (assert-non-nil (ppcre:scan "^FDOG/OK" response))))

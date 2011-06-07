c55008a556380df5e3e23395ff76c3c7aea72a04
Yaroslav Shirokov <slava@vitrue.com>
2011-06-07 01:43:39 -0400

    [forwarder] Generate new ports for listeners. TODO: Make the generation consider all of mongrel2's existing bindings and avoiding them.

--- src/services/forwarder/utils.lisp
+++ src/services/forwarder/utils.lisp
@@ -1,13 +1,19 @@
 (in-package :fdog-forwarder)
 
+(defvar next-forwarder-port 50000)
+(defun next-forwarder-port (&key reset)
+  (when reset
+    (setf next-forwarder-port 5000))
+  (incf next-forwarder-port))
+
 (defun make-handler-send-spec ()
-  "tcp://127.0.0.1:59910")
+  (format nil "tcp://127.0.0.1:~A" (next-forwarder-port)))
 
 (defmethod make-handler-send-ident ((forwarder fdog-forwarder))
   (format nil "forwarder-~A" (fdog-forwarder-name forwarder)))
 
 (defun make-handler-recv-spec ()
-  "tcp://127.0.0.1:29999")
+  (format nil "tcp://127.0.0.1:~A" (next-forwarder-port)))
 
 (defmethod make-handler-recv-ident ((forwarder fdog-forwarder))
   "")

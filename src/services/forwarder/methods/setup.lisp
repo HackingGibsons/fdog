1a457479b4b04ed89bcdf61e21c5b8c0a0013556
Yaroslav Shirokov <slava@vitrue.com>
2011-06-19 06:50:06 -0400

    [forwarder-mongrel-setup] Sketching up the setup.  Adding iterators of hosts and a fetcher of all participating servers

--- src/services/forwarder/methods/setup.lisp
+++ src/services/forwarder/methods/setup.lisp
@@ -1,5 +1,20 @@
 (in-package :fdog-forwarder)
 
-(defmethod configure-forwarder ((forwarder fdog-forwarder))
-  "Configure the forwarder mongrel to include `forwarder'"
-  :undef)
+(defun forwarder-servers ()
+  "A list of all servers that are configured for forwarding"
+  (multiple-value-list (ensure-forwarder-servers-exist)))
+
+(defmethod configure-forwarder ((forwarder fdog-forwarder) (server mongrel2-server))
+  "Configure `forwarder' on `server'"
+  (log-for (trace) "Configuring ~A on ~A" forwarder (fdog-models:mongrel2-server-name server))
+  (dolist (hostpath (fdog-forwarder-hostpaths forwarder))
+    (log-for (warn) "TODO: Should configure ~A => ~A"
+             (fdog-hostpath-host hostpath)
+             (fdog-hostpath-path hostpath))))
+
+(defmethod configure-forwarder ((forwarder fdog-forwarder) (server (eql :all)))
+  "Configure all forwarder servers to include `forwarder'"
+  (log-for (trace) "Configuring ~A on all forwarder servers" forwarder)
+  (mapcar #'(lambda (s) (configure-forwarder forwarder s))
+          (forwarder-servers)))
+

2098e3c83f963235800aa32027491458de722d39
Yaroslav Shirokov <slava@vitrue.com>
2011-06-19 20:15:26 -0400

    [forwarder-interface] Scaffolding forwarder initialization

--- src/services/forwarder/package.lisp
+++ src/services/forwarder/package.lisp
@@ -67,3 +67,63 @@
 (defvar *forwarders* ()
   "List of the loaded forwarders")
 
+
+;; Forwarder engine, that which makes the definitions dance
+(defclass forwarder-engine ()
+  ((forwarder :initarg :forwarder
+              :accessor forwarder-engine-forwarder)
+   (servers :initarg :servers
+            :initform (forwarder-servers)
+            :accessor forwarder-engine-servers))
+  (:documentation "The engine that manages the forwarding of requests for an endpoint"))
+
+;; Engine creation
+(defmethod make-forwarder-engine ((forwarder fdog-forwarder) &key servers)
+  "Construct a forwarder-engine class from an fdog-forwarder model `forwarder'"
+  (log-for (trace) "Building forwarder engine from ~A" forwarder)
+  (make-instance 'forwarder-engine :forwarder forwarder :servers servers))
+
+;; Engine state control
+(defgeneric forwarder-engine-running-p (engine)
+  (:documentation "Boolean representation of engine operation")
+  (:method ((engine forwarder-engine))
+    (log-for (warn) "TODO: Not implemented")
+    nil))
+
+(defgeneric forwarder-engine-start (engine)
+  (:documentation "Start forwarder engine `engine' so it begins to serve requests.")
+  (:method ((engine (eql :all)))
+    (mapc #'forwarder-engine-start *forwarders*)))
+
+(defgeneric forwarder-engine-stop (engine)
+  (:documentation "Stop forwarder engine `engine' so it ceases to serve requests.")
+  (:method ((engine (eql :all)))
+    (mapc #'forwarder-engine-stop *forwarders*)))
+
+(defmethod forwarder-engine-start ((engine forwarder-engine))
+  (log-for (trace) "Starting engine: ~A" engine)
+  :undef)
+
+(defmethod forwarder-engine-stop ((engine forwarder-engine))
+  (log-for (trace) "Shutting down engine: ~A" engine)
+  :undef)
+
+
+;; General forwarders API
+(defmethod init-forwarders (&key (start t))
+  (let ((forwarder-servers (ensure-forwarder-environment))
+        (forwarders (find-forwarder)))
+
+    (log-for (trace) "Stopping all ~A existing forwarders" (length *forwarders*))
+    (forwarder-engine-stop :all)
+
+    (log-for (trace) "Configuring all ~A found forwarders" (length forwarders))
+    (configure-forwarder :all :all)
+
+    (setf *forwarders* (mapcar #'(lambda (f) (make-forwarder-engine f :servers forwarder-servers))
+                               (remove-if-not #'forwarder-valid-p forwarders)))
+    (log-for (trace) "Built ~A/~A engines" (length *forwarders*) (length forwarders))
+
+    (when start
+      (log-for (trace) "Starting all forwarder engines")
+      (forwarder-engine-start :all))))

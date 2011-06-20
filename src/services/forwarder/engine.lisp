4f29f93722fa3890c28da95d85947ba96274c1d7
Yaroslav Shirokov <slava@vitrue.com>
2011-06-20 12:09:40 -0400

    [forwarder-engine] Building all the handler bridges

--- src/services/forwarder/engine.lisp
+++ src/services/forwarder/engine.lisp
@@ -6,14 +6,37 @@
               :accessor forwarder-engine-forwarder)
    (servers :initarg :servers
             :initform (forwarder-servers)
-            :accessor forwarder-engine-servers))
+            :accessor forwarder-engine-servers)
+   (bridges :initarg :bridges
+            :initform ()
+            :accessor forwarder-engine-bridges))
   (:documentation "The engine that manages the forwarding of requests for an endpoint"))
 
 ;; Engine creation
 (defmethod make-forwarder-engine ((forwarder fdog-forwarder) &key servers)
   "Construct a forwarder-engine class from an fdog-forwarder model `forwarder'"
   (log-for (trace) "Building forwarder engine from ~A" forwarder)
-  (make-instance 'forwarder-engine :forwarder forwarder :servers servers))
+  (let ((engine (make-instance 'forwarder-engine :forwarder forwarder :servers servers)))
+    (init-bridges engine)
+    engine))
+
+(defmethod init-bridges ((engine forwarder-engine))
+  (log-for (trace) "Initializing bridges for engine: ~A" engine)
+  (with-slots (forwarder servers bridges) engine
+    (let ((paths (forwarder-uniqe-paths forwarder))
+          handler route path)
+      (log-for (trace) "Forwarder has paths: ~A" paths)
+      (dolist (path paths bridges)
+        (dolist (server servers)
+          (setf handler (find-mongrel2-handler
+                         :send-ident (send-ident-for forwarder path (mongrel2-server-ssl-p server)))
+                route (mongrel2-target-route handler)
+                path (mongrel2-route-path route))
+          (log-for (trace) "Configuring handler: ~A" handler)
+          (log-for (trace) "Route template: ~A" route)
+          (log-for (trace) "Path to strip in handler: ~A" path)
+          (push (configure-bridges-for handler) bridges)
+          (log-for (warn) "TODO: Configure a handler lambda"))))))
 
 ;; Engine state control
 (defgeneric forwarder-engine-running-p (engine)

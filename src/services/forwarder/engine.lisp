e138e09552eee0e424d333c77a7edafc9ed59f67
Yaroslav Shirokov <slava@vitrue.com>
2011-06-21 14:13:10 -0400

    [forwarder-engine] Socket creating, have proxy and client sockets bound

--- src/services/forwarder/engine.lisp
+++ src/services/forwarder/engine.lisp
@@ -1,6 +1,7 @@
 (in-package :fdog-forwarder)
 
 ;; Forwarder endpoint, a proxy between the handlers and an outside consumer
+(defvar *endpoint-socket-linger* 1000)
 (defclass forwarder-engine-endpoint ()
   ((engine :initarg :engine
            :accessor endpoint-engine)
@@ -18,15 +19,15 @@
    ;; Proxies
    (request-proxy-sock-addr :initarg :proxy-addr
                             :accessor endpoint-proxy-addr)
-   (request-proxy-sock :initarg :proxy-sock
+   (request-proxy-sock :initarg :proxy-sock :initform nil
                        :accessor endpoint-proxy-sock)
-   (response-process-sock :initarg :response-process-sock
+   (response-process-sock :initarg :response-process-sock :initform nil
                           :accessor endpoint-response-process-sock)
 
    ;; Public facing sockets
-   (request-sock :initarg :push-sock
+   (request-sock :initarg :push-sock :initform nil
                  :accessor endpoint-request-sock)
-   (response-sock :initarg :sub-sock
+   (response-sock :initarg :sub-sock :initform nil
                   :accessor endpoint-response-sock)))
 
 ;; Multibridge, interface for running multiple bridges at the same time
@@ -63,35 +64,78 @@
             path (mongrel2-handler-send-ident handler))))
 
 ;; Forwarder endpoint creation
-(defmethod terminate-context ((endpoint forwarder-engine-endpoint))
-  (when (endpoint-context endpoint)
-    (log-for (trace) "Terminating endpoint context: ~A" endpoint)
-    (zmq:term (endpoint-context endpoint))
-    (setf (endpoint-context endpoint) nil)
-    (log-for (trace) "Terminated: ~A" endpoint)))
-
-(defmethod terminate-sockets ((endpoint forwarder-engine-endpoint))
-  (log-for (warn) "TODO: terminate-sockets is a stub.")
-  (values nil
-          :undef))
-
 (defmethod init-context ((endpoint forwarder-engine-endpoint))
   (terminate-context endpoint)
   (log-for (trace) "Creating ZMQ context for endpoint with ~A threads" (endpoint-context-threads endpoint))
   (setf (endpoint-context endpoint)
         (zmq:init (endpoint-context-threads endpoint))))
 
+(defmethod terminate-context ((endpoint forwarder-engine-endpoint))
+  (when (endpoint-context endpoint)
+    (terminate-sockets endpoint)
+    (log-for (trace) "Terminating endpoint context: ~A" endpoint)
+    (zmq:term (endpoint-context endpoint))
+    (setf (endpoint-context endpoint) nil)
+    (log-for (trace) "Terminated: ~A" endpoint)))
+
+(defmethod forwarder-engine-forwarder ((endpoint forwarder-engine-endpoint))
+  (forwarder-engine-forwarder (endpoint-engine endpoint)))
+
+(defmethod maybe-linger-socket (socket)
+  (and *endpoint-socket-linger*
+       (zmq:setsockopt socket zmq:linger *endpoint-socket-linger*))
+  socket)
+
 (defmethod init-sockets ((endpoint forwarder-engine-endpoint))
-  (log-for (warn) "TODO: init-sockets is a stub.")
-  (values nil
-          :undef))
+  (log-for (trace) "Initializing sockets of endpoing ~A" endpoint)
+  (terminate-sockets endpoint)
+
+  (with-slots (context) endpoint
+
+    ;; Client socks
+    (with-slots (request-sock response-sock) endpoint
+      (let* ((forwarder (forwarder-engine-forwarder endpoint))
+             (req-addr (make-local-endpoint :port (fdog-forwarder-forward-to forwarder)))
+             (res-addr (make-local-endpoint :port (fdog-forwarder-listen-on forwarder))))
+        (setf request-sock
+              (maybe-linger-socket (zmq:socket context zmq:push))
+              response-sock
+              (maybe-linger-socket (zmq:socket context zmq:sub)))
+
+        (log-for (trace) "Binding forwarder [~A] client sockets Req: ~A and Rep: ~A"
+                 (fdog-forwarder-name forwarder) req-addr res-addr)
+        (zmq:setsockopt response-sock zmq:subscribe "")
+        (zmq:bind request-sock req-addr)
+        (zmq:bind request-sock res-addr)
+        (log-for (trace) "Client socket binding complete.")))
+
+
+
+    ;; Request proxy
+    (with-slots (request-proxy-sock-addr request-proxy-sock) endpoint
+      (setf request-proxy-sock-addr
+            (make-local-endpoint :addr "127.0.0.1" :port (next-handler-port))) ;; TODO: Use a different generator than next-handler-port
+      (log-for (trace) "I have chosen the address: ~A as the request-proxy, binding" request-proxy-sock-addr)
+      (setf request-proxy-sock
+            (maybe-linger-socket (zmq:socket context zmq:pull)))
+
+      (zmq:bind request-proxy-sock request-proxy-sock-addr)
+      (log-for (trace) "Request proxy bound."))))
+
+(defmethod terminate-sockets ((endpoint forwarder-engine-endpoint))
+  (log-for (trace) "Terminating sockets of endpoint: ~A" endpoint)
+  (with-slots (request-proxy-sock response-process-sock request-sock response-sock) endpoint
+    (mapcar #'zmq:close (remove nil (list request-proxy-sock response-process-sock request-sock response-sock)))
+    (setf request-proxy-sock nil
+          response-process-sock nil
+          request-sock nil
+          response-sock nil)))
 
 ;; Forwarder endpoint operation
 (defmethod engine-endpoint-running-p ((endpoint forwarder-engine-endpoint))
   (terminate-context endpoint)
   (with-slots (processor) endpoint
-    (and (endpoint-context endpoint)
-         processor
+    (and processor
          (threadp processor)
          (thread-alive-p processor))))
 
@@ -112,9 +156,11 @@
 
 (defmethod engine-endpoint-stop ((endpoint forwarder-engine-endpoint))
   (log-for (trace) "Stopping endpoint: ~A" endpoint)
+  (log-for (trace) "Running: ~A" (engine-endpoint-running-p endpoint))
+  (terminate-sockets endpoint)
+  (terminate-context endpoint)
   (when (engine-endpoint-running-p endpoint)
-    (terminate-sockets endpoint)
-    (terminate-context endpoint)
+    (log-for (trace) "Preparing to terminate thread.")
     (log-for (warn) "This is very much the wrong way to go about this one.")
     (destroy-thread (endpoint-processor endpoint))
     :stopped))

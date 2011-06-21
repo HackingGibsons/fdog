ac546956429eede5d60beddadd9781db5b2ce218
Yaroslav Shirokov <slava@vitrue.com>
2011-06-21 13:12:02 -0400

    [forwarder-engine] Fix handler port collision

--- src/cli/configuration.lisp
+++ src/cli/configuration.lisp
@@ -4,9 +4,9 @@
   (using-configuration!
    (with-server ("control" :bind "0.0.0.0" :port 1337 :chroot "./")
      (with-host ("localhost")
-       (make-route "/" (make-handler :send-spec "tcp://127.0.0.1:13375" ;; 5 => S => Send, Get it?
+       (make-route "/" (make-handler :send-spec "tcp://127.0.0.1:1335" ;; 5 => S => Send, Get it?
                                      :send-ident "control-ident"
-                                     :recv-spec "tcp://127.0.0.1:13372")) ;; 2 => R => Recieve, right? ..right? :(
+                                     :recv-spec "tcp://127.0.0.1:1332")) ;; 2 => R => Recieve, right? ..right? :(
        (make-route "/static/" (make-dir "./public/")))))
 
   (fdog-models:make-mongrel2-setting :certdir "certs/"))

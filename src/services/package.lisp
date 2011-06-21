bf49f5123203b50249396514eadd5d58d3e5767d
Yaroslav Shirokov <slava@vitrue.com>
2011-06-21 13:54:24 -0400

    [forwarder-engine] Starting forwarders when we boot

--- src/services/package.lisp
+++ src/services/package.lisp
@@ -6,4 +6,5 @@
 ;; TODO: Services could specialize :afters on this instead of me
 ;;       piling up a bunch of services here
 (defmethod init-services (&rest services)
-  (fdog-control:init-control-interface))
+  (fdog-control:init-control-interface)
+  (fdog-forwarder:init-forwarders))

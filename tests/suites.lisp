9a2c874f68ab09a71499b6fb4b4dc5a26511a97b
Yaroslav Shirokov <slava@vitrue.com>
2011-07-07 17:53:12 -0400

    [tests-catchup] Cut off useless comment

--- tests/suites.lisp
+++ tests/suites.lisp
@@ -1,22 +1,5 @@
 (in-package :fdog-tests)
 
-;; ;; Grouping master-suites
-;; (def-suite main :description "'The' test suite for fdog")
-;; (def-suite mongrel2 :in main
-;;            :description "A grouping of mongrel2 tests")
-
-
-;; ;; Actual test-grouping suites
-;; (def-suite basic :in main
-;;            :description "Basic tests, bordering on sanity checks")
-
-;; (def-suite mongrel2/db :in mongrel2
-;;            :description "Basic tests that involve Mongrel2")
-
-;; (def-suite mongrel2/proc :in mongrel2
-;;            :description "Tests that involve a running Mongrel2")
-
-
 (def-test-group database-basic-tests ()
   (:documentation "Database baisc tests"))
 

5434e99e5c39871fea031eb03a2581b311695908
Yaroslav Shirokov <slava@vitrue.com>
2011-07-29 13:48:03 -0400

    [safer-recvs] Safe recv! function in the style of the earlier send!

--- src/services/forwarder/utils.lisp
+++ src/services/forwarder/utils.lisp
@@ -88,9 +88,21 @@ rather than panic and fall down crying. >:["
 of EINTR recursively.  Second value returned is the number of times the operation was retried."
   (let* ((res (zmq:send sock msg flags))
          (res (cond ((and (= res -1)
-                        (= (sb-alien:get-errno) sb-posix:eintr))
-                   (send! sock msg flags (1+ count)))
+                          (= (sb-alien:get-errno) sb-posix:eintr))
+                     (send! sock msg flags (1+ count)))
 
-                  (:otherwise res))))
+                    (:otherwise res))))
+    (values res count)))
+
+(export 'recv!)
+(defun recv! (sock msg &optional flags (count 0))
+  "Keep trying to `zmq:recv' while it keeps returning -1 with an errno
+of EINTR or EFSM recursively.  Second value returned is the number of times the operation was retried."
+  (let* ((res (zmq:recv sock msg flags))
+         (res (cond ((and (= res -1)
+                          (member (sb-alien:get-errno) `(,sb-posix:eintr ,zmq:efsm)))
+                     (recv! sock msg flags (1+ count)))
+
+                    (:otherwise res))))
     (values res count)))
 

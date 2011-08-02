d595f95099bbf63a6f055705ed0f1b70f0f821db
Yaroslav Shirokov <slava@vitrue.com>
2011-08-02 14:14:28 -0400

    [tests-catchup] The scaffold functions as intended, I can now build on top

--- tests/make/scaffold.mk
+++ tests/make/scaffold.mk
@@ -4,8 +4,7 @@ TEST_PATH = $(ROOT)/$(TEST_DIR)/
 
 build-test-fdog:
 	@echo "=> Building a test server in: $(TEST_PATH)"
-	$(LISP) --no-userinit \
-	        --load $(QL_ROOT_PATH)/setup.lisp \
+	$(LISP) --load $(QL_ROOT_PATH)/setup.lisp \
 	        --eval '(ql:quickload :fdog)' \
 			--eval '(fdog-cli:fdog-main `("fdog-test" "init" "--no-input" "$(TEST_PATH)"))' \
 			--eval '(quit)'
@@ -13,13 +12,26 @@ $(TEST_DIR): build-test-fdog
 
 start-test-fdog: $(TEST_DIR)
 	@echo "=> Starting instance in $(TEST_PATH)"
-	$(LISP) --no-userinit \
-	        --load $(QL_ROOT_PATH)/setup.lisp \
-	        --eval '(ql:quickload :fdog)' \
-			--eval '(fdog-cli:fdog-main `("fdog-test" "start" "$(TEST_PATH)"))'
+	( \
+	 cd $(TEST_PATH); \
+	 nohup $(LISP) --load $(QL_ROOT_PATH)/setup.lisp \
+	 	   --eval '(ql:quickload :fdog)' \
+		   --eval '(fdog-cli:fdog-main `("fdog-test" "start" "$(TEST_PATH)" "--no-fork"))' \
+		   --eval '(quit)' & \
+	);
+	@echo "=> Sleeping to ensure test server boot."
+	@sleep 6
 
 stop-test-fdog:
 	@echo "=> Stopping instance in $(TEST_PATH)"
+	$(LISP) --load $(QL_ROOT_PATH)/setup.lisp \
+	        --eval '(ql:quickload :fdog)' \
+			--eval '(fdog-cli:fdog-main `("fdog-test" "stop" "$(TEST_PATH)"))' \
+			--eval '(quit)'
+	@echo "-> Killing stray mongrels"
+	for m2pid in $$(find $(TEST_PATH)/server/run/ -name '*.pid'); do \
+	 	 cat $$m2pid | xargs kill ; \
+	done
 	$(MAKE) clean-test-fdog
 
 clean-test-fdog:

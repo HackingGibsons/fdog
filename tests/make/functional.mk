1ae6d4fd6d811182563a1a7ae7dad5a8757f2831
Yaroslav Shirokov <slava@vitrue.com>
2011-08-02 15:46:08 -0400

    [unstable] Backslashes. How do they work?

--- tests/make/functional.mk
+++ tests/make/functional.mk
@@ -6,7 +6,7 @@ run-tests-functional: start-test-fdog
 	        --load $(QL_ROOT_PATH)/setup.lisp \
 	        --eval '(ql:quickload :fdog)' \
 	        --eval '(ql:quickload :fdog-tests)' \
-	        --eval '(funcall (intern (symbol-name :start-logging) :fdog))'
+	        --eval '(funcall (intern (symbol-name :start-logging) :fdog))' \
 			--eval '(funcall (intern (symbol-name :run-functional) :fdog-tests))' \
 	        --eval '(quit)'
 	@echo "Terminating functional test scaffold."

61d2a99de7eb210854af1d58835cdc4e237de437
Yaroslav Shirokov <slava@vitrue.com>
2011-08-03 12:50:09 -0400

    [tests-catchup] Add the timeout directly to the http requester.

--- tests/helpers/utils.lisp
+++ tests/helpers/utils.lisp
@@ -5,14 +5,18 @@
 Mostly exists to close the stream when required and repackage the 7-value
 return as something more edible (plist). Though discarding some values that
 are less usefull to string construction."
-  (multiple-value-bind
-        (response status-code headers uri stream must-close reason-phrase)
-        (apply 'drakma:http-request `(,url ,@args))
+  (bt:with-timeout (3)
+    (handler-case
+        (multiple-value-bind
+              (response status-code headers uri stream must-close reason-phrase)
+              (apply 'drakma:http-request `(,url ,@args))
 
-    (when must-close (close stream))
+          (when must-close (close stream))
 
-    (values response
-            (list :status-code status-code
-                  :headers     headers
-                  :uri         uri
-                  :reason      reason-phrase))))
+          (values response
+                  (list :status-code status-code
+                        :headers     headers
+                        :uri         uri
+                        :reason      reason-phrase)))
+
+      (bt:timeout () nil))))

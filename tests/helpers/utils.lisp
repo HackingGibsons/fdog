69e667c3296deaddbd3c681d0d33bc36d84a2bfd
Yaroslav Shirokov <slava@vitrue.com>
2011-08-04 16:26:54 -0400

    [tests-catchup] better http->json

--- tests/helpers/utils.lisp
+++ tests/helpers/utils.lisp
@@ -1,15 +1,17 @@
 (in-package :fdog-tests)
 
-(defun http->string (url &rest args)
+(defun http->string (url &key (timeout 3))
   "A slightly repackaged veresion of drakma's client.
 Mostly exists to close the stream when required and repackage the 7-value
 return as something more edible (plist). Though discarding some values that
-are less usefull to string construction."
-  (bt:with-timeout (3)
+are less usefull to string construction.
+The first value returned is only a string if drakma thinks it is, otherwise
+it will be an octet vector"
+  (bt:with-timeout (timeout)
     (handler-case
         (multiple-value-bind
               (response status-code headers uri stream must-close reason-phrase)
-              (apply 'drakma:http-request `(,url ,@args))
+              (let (args) (apply 'drakma:http-request `(,url ,@args)))
 
           (when must-close (close stream))
 
@@ -20,6 +22,16 @@ are less usefull to string construction."
                         :reason      reason-phrase)))
 
       (bt:timeout () nil))))
+
+(defun http->json (url &rest keys)
+  "Wrap `http->string' to parse out JSON, and act more leniently in the face
+of octet vectors."
+  (multiple-value-bind (res meta) (apply #'http->string `(,url ,@keys))
+    (values (typecase res
+              (string (json:decode-json-from-string res))
+              (vector (json:decode-json-from-string (flex:octets-to-string res)))
+              (otherwise res))
+            meta)))
 
 (defun make (target &key (dir (asdf:system-source-directory :fdog)))
   "Wrapper for executing gnumake on a target. Defaults to running in the `:fdog' package home,

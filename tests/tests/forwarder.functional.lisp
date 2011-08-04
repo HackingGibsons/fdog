e11162e839dc9330f375d2435266666bc169b9a6
Yaroslav Shirokov <slava@vitrue.com>
2011-08-04 18:05:18 -0400

    [tests-catchup] Testing the proper construction of a forwarder and the querying of the endpoints it should use for the same.

--- tests/tests/forwarder.functional.lisp
+++ tests/tests/forwarder.functional.lisp
@@ -9,6 +9,7 @@
 (defun assert-response-200 (meta)
   (assert-equal 200 (getf meta :status-code)))
 
+
 ;; Tests
 (def-test+func (can-hit-slash) :eval
   (multiple-value-bind (res meta)  (http->string "http://localhost:1337/")
@@ -18,4 +19,17 @@
 (def-test+func (can-create-forwarder-and-query-for-it) :eval
   (multiple-value-bind (res meta)  (http->json "http://localhost:1337/api/forwarders/")
     (assert-null res)
-    (assert-response-200 meta)))
+    (assert-response-200 meta))
+
+  (let ((req '((:name . "test") (:hosts . (("localhost" . "/test/"))))))
+    (multiple-value-bind (res meta) (http->json "http://localhost:1337/api/forwarders/create/" :method :POST
+                                                :content (json:encode-json-to-string req))
+      (assert-non-nil res)
+      (assert-response-200 meta)))
+
+  (multiple-value-bind (res meta) (http->json "http://localhost:1337/api/forwarders/test/")
+    (assert-response-200 meta)
+    (assert-non-nil res)
+
+    (assert-non-nil (assoc :push res :test #'string-equal))
+    (assert-non-nil (assoc :sub res :test #'string-equal))))

9c73c01376f20f5d157770f83b22c5b294029a12
Yaroslav Shirokov <slava@vitrue.com>
2011-08-04 16:53:38 -0400

    [tests-catchup] Correctly hitting the server, and less insane condition checking

--- tests/tests/forwarder.functional.lisp
+++ tests/tests/forwarder.functional.lisp
@@ -1,14 +1,21 @@
 (in-package :fdog-tests)
 ;; Helpers
-(defmacro def-test+func (name &body body)
+(defmacro def-test+func ((name &key fixtures) &body body)
   "Utility to shorten the process of writing an fdog functional test"
   `(def-test (,name :group fdog-forwarder-functional-tests
-                    :fixtures (fdog/functional))
+                    :fixtures (fdog/functional ,@fixtures))
        ,@body))
 
-(def-test+func can-hit-slash :eval
-  (let* ((url (format nil "http://~A:~A/" "localhost" 1337))
-         (res (http->string url)))
-    (assert-non-nil res)))
+(defun assert-response-200 (meta)
+  (assert-equal 200 (getf meta :status-code)))
 
+;; Tests
+(def-test+func (can-hit-slash) :eval
+  (multiple-value-bind (res meta)  (http->string "http://localhost:1337/")
+    (assert-non-nil res)
+    (assert-response-200 meta)))
 
+(def-test+func (can-create-forwarder-and-query-for-it) :eval
+  (multiple-value-bind (res meta)  (http->json "http://localhost:1337/api/forwarders/")
+    (assert-null res)
+    (assert-response-200 meta)))

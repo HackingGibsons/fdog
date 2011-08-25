bb5c317a871eac1b9a04be5f25b11a799d21cfac
Yaroslav Shirokov <slava@hackinggibsons.com>
2011-08-24 22:24:37 -0400

    [master] Logging

---
+++ src/logging.lisp
@@ -0,0 +1,13 @@
+(in-package :afdog)
+
+(log5:defoutput human-time (multiple-value-bind (second minute hour date month year)
+                               (decode-universal-time (get-universal-time))
+                             (format nil "[~D-~2,'0D-~2,'0D ~2,'0D:~2,'0D:~2,'0D]" year month date hour minute second)))
+
+
+(defun start-logging (&key (default t))
+  (when default
+    (log5:start-sender 'default
+                       (log5:stream-sender :location *error-output*)
+                       :category-spec '(log5:dribble+)
+                       :output-spec '(human-time log5:category log5:message))))

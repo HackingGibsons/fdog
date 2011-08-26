d340916c0a633159775bed6e15172ef877bbf190
Yaroslav Shirokov <slava@hackinggibsons.com>
2011-08-25 21:58:22 -0400

    [master] standard-organ now opens connects and disconnects sockets from the bus during the agent lifecycle

--- src/agent/classes/standard-organ.lisp
+++ src/agent/classes/standard-organ.lisp
@@ -5,4 +5,9 @@
           :accessor organ-agent)
    (uuid :initarg :uuid
          :reader organ-uuid
-         :initform (print-object (uuid:make-v4-uuid) nil))))
+         :initform (print-object (uuid:make-v4-uuid) nil))
+
+   (incoming-sock :initform nil
+                  :accessor organ-incoming-sock)
+   (outgoing-sock :initform nil
+                  :accessor organ-outgoing-sock)))
--- src/agent/methods/standard-organ.lisp
+++ src/agent/methods/standard-organ.lisp
@@ -3,3 +3,39 @@
 (defmethod agent-tick ((organ standard-organ) event)
   "By default, an organ tick is a no-op"
   nil)
+
+(defmethod agent-boot ((agent standard-agent) (organ standard-organ) &rest options)
+  (declare (ignorable options))
+  (call-next-method)
+  (log-for (trace) "Organ specific boot of ~A for ~A" organ agent)
+
+  (log-for (trace) "Building and connecting sockets.")
+  (setf (organ-incoming-sock organ)
+        (zmq:socket (agent-context agent) zmq:sub)
+
+        (organ-outgoing-sock organ)
+        (zmq:socket (agent-context agent) zmq:pub))
+
+  (log-for (trace) "Connecting the incoming socket: ~A" (agent-message-addr agent))
+  (zmq:connect (organ-incoming-sock organ) (agent-message-addr agent))
+  (log-for (warn) "Subscribing incoming socket to everything.")
+  (zmq:setsockopt (organ-incoming-sock organ) zmq:subscribe "")
+
+  (log-for (trace) "Connecting the outgoing socket: ~A" (agent-event-addr agent))
+  (zmq:connect (organ-outgoing-sock organ) (agent-event-addr agent))
+
+  organ)
+
+(defmethod agent-disconnect :before ((agent standard-agent) (organ standard-organ) &rest options)
+  (declare (ignorable options))
+  (log-for (trace) "Organ specific :before disconnect of ~A from ~A" organ agent)
+
+  (log-for (trace) "Closing outgoing sock.")
+  (when (organ-outgoing-sock organ)
+    (zmq:close (organ-outgoing-sock organ))
+    (setf (organ-outgoing-sock organ) nil))
+
+  (log-for (trace) "Closing incoming sock.")
+  (when (organ-incoming-sock organ)
+    (zmq:close (organ-incoming-sock organ))
+    (setf (organ-incoming-sock organ) nil)))

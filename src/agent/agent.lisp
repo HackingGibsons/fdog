9d6c5e940d2ea5dbe6f1ea8254451130b268d32e
Yaroslav Shirokov <slava@hackinggibsons.com>
2011-08-26 01:45:22 -0400

    [master] Adding an appendix to the agent scaffold

--- src/agent/agent.lisp
+++ src/agent/agent.lisp
@@ -5,10 +5,16 @@
   (let ((heart (make-instance 'agent-heart :agent agent)))
     heart))
 
+(defmethod make-appendix-for ((agent standard-agent))
+  (let ((appendix (make-instance 'agent-appendix :agent agent)))
+    appendix))
+
 (defun make-agent ()
   "Agent maker wrapper"
   (let* ((agent (make-instance 'standard-agent))
-         (heart (make-heart-for agent)))
+         (heart (make-heart-for agent))
+         (appendix (make-appendix-for agent)))
     (agent-connect agent heart)
+    (agent-connect agent appendix)
     agent))
 

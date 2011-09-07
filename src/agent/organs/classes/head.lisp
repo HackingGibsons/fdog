7923d424cafac319b86b505a4dee967b079dd3db
Yaroslav Shirokov <slava@vitrue.com>
2011-09-07 13:01:08 -0400

    [afdog] Handler for beat events form systems

--- src/agent/organs/classes/head.lisp
+++ src/agent/organs/classes/head.lisp
@@ -1,8 +1,27 @@
 (in-package :agent)
 
-(defclass agent-head (standard-organ)
-  ()
-  (:documentation "This organ is responsible for decision making and task scheduling, but none of the work."))
+(defclass agent-head (standard-beating-organ)
+  ((organs :initform (make-hash-table :test 'equalp)
+           :accessor agent-organs))
 
-(defmethod act-on-event ((organ agent-head) event)
-  (log-for (warn) "THIS HEAD DOES NOTHING: [~A]::~A" (type-of event) event))
+  (:documentation "This organ is responsible for decision making and task scheduling, but none of the work.")
+  (:default-initargs . (:tag :head)))
+
+(defmethod act-on-event ((head agent-head) event)
+  (prog1 event
+    (cond ((and (listp event) (getf (reverse event) :beat))
+           (organ-beat-event head event))
+          (:otherwise
+           (log-for (warn) "~A doesn't deal with ~A" head event)))))
+
+(defmethod organ-beat-event ((head agent-head) (event list))
+  "Handle the updating of an organ's state."
+  (let ((tag (car event))
+        (uuid (getf event :uuid))
+        (time (getf event :time)))
+
+    (unless (and tag uuid time)
+      (log-for (warn) "~A: organ-beat-event did nat have all of: tag, :uuid or :time" head)
+      (error "organ-beat-event did nat have all of: tag, :uuid or :time"))
+
+    (log-for (trace) "Updating organ: ~A/~A @ ~A" tag uuid time)))

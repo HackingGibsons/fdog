f466c37104f4d58712d1878b6f1758b78da0bf40
Yaroslav Shirokov <slava@hackinggibsons.com>
2011-09-20 13:26:39 -0400

    [master] Funcallable behavior is go.

--- src/agent/organs/classes/head.lisp
+++ src/agent/organs/classes/head.lisp
@@ -7,7 +7,7 @@
   (:documentation "This organ is responsible for decision making and task scheduling, but none of the work.")
   (:default-initargs . (:tag :head)))
 
-(defclass standard-behavior ()
+(defclass standard-behavior (c2mop:funcallable-standard-object)
   ()
   (:metaclass c2mop:funcallable-standard-class))
 
@@ -15,7 +15,14 @@
   "Generates a behavior function with a description of how it should be invoked."
   `(progn
      (defclass ,name (standard-behavior)
-       ())))
+       ()
+       (:metaclass c2mop:funcallable-standard-class))
+
+     (defmethod initialize-instance :after ((behavior ,name) &key)
+       (c2mop:set-funcallable-instance-function
+        behavior
+        #'(lambda ,invoke-lambda (progn (log-for (trace) "Behavior ~A running" (symbol-name ',name))
+                                        ,@body))))))
 
 (defbehavior announce-self (:interval (:from :heart :nth 3) :do :invoke) (organ)
   :oh-hello)

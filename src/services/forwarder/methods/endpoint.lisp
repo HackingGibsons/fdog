8558c675bd590cf11fb089b8067d0730563f8f94
Reed Lipman <rmlipman@gmail.com>
2011-09-22 12:18:58 -0400

    Bind response proxy address

--- src/services/forwarder/methods/endpoint.lisp
+++ src/services/forwarder/methods/endpoint.lisp
@@ -67,10 +67,16 @@
       (log-for (trace) "Request proxy bound."))
 
     ;; Response proxy
-    (with-slots (response-proxy-sock) endpoint
+    (with-slots (response-proxy-addr response-proxy-sock) endpoint
+      (setf response-proxy-addr
+            (make-local-endpoint :addr "127.0.0.1" :port (next-handlerport))) ;; TODO: see above
+      (log-for (trace) "Chosen the address: ~A as the response-proxy, binding" response-proxy-addr)
       (setf response-proxy-sock
             (maybe-linger-socket (zmq:socket context zmq:pub)))
       (log-for (trace) "Connecting reply socket to relevant M2 endpoints.")
+      (zmq:bind response-proxy-sock response-proxy-addr)
+      (log-for (trace) "Response proxy bound.")
+
       (dolist (addr (mapcar #'mongrel2-handler-recv-spec
                             (forwarder-engine-handlers (endpoint-engine endpoint))))
         (log-for (trace) "  Connecting to: ~A" addr)

288719c1ab336eda7021884a5284da9845336d65
Yaroslav Shirokov <slava@hackinggibsons.com>
2011-09-30 15:02:27 -0400

    [master] Announce self behavior now announces itself.

--- src/agent/behavior/behaviors/peers.lisp
+++ src/agent/behavior/behaviors/peers.lisp
@@ -1,5 +1,8 @@
 (in-package :agent)
 
 (defbehavior announce-self (:interval (:from :heart :nth 3) :do :invoke) (organ)
-  (log-for (warn) "TODO: Running behavior lambda for ~A" organ)
-  :oh-hello)
+  (log-for (warn) "TODO: Running ~A behavior lambda for ~A" behavior organ)
+  (send-message organ `(,(organ-tag organ) :command
+                         :command :speak
+                         :say (:agent :info :info ,(agent-info (organ-agent organ)))))
+  (log-for (warn) "Message sent."))

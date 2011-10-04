1c30c0fd655985d72bf12a8232e14ae3eff5a676
Yaroslav Shirokov <slava@hackinggibsons.com>
2011-10-04 15:56:27 -0400

    [master] Moving the local socket util to utils

--- src/agent/organs/classes/mouth.lisp
+++ src/agent/organs/classes/mouth.lisp
@@ -11,16 +11,6 @@
   (:documentation "Responsible for outgoing cross-agent communication.")
   (:default-initargs . (:tag :mouth)))
 
-(defmethod make-local-sock (context type &key port)
-  "Make a locally bound socket of type `type' within `context' using the `transport'
-type. Returns two values: the socket created and the address that was bound to in `zmq:connect' format"
-  (let ((sock (zmq:socket context type))
-        (addr (format nil "tcp://~A:~A" (get-local-address :as :string) (+ 50000 (random 10000)))))
-    ;; TODO: ^^ Port generation sucks. Bind and try :O
-    (log-for (warn) "TODO: Local sock on ~A" addr)
-
-    (values sock addr)))
-
 (defmethod agent-boot :after ((agent standard-agent) (mouth agent-mouth) &rest options)
   "Mouth specific socket inits."
   (declare (ignorable options))
--- src/utils.lisp
+++ src/utils.lisp
@@ -22,3 +22,19 @@ If `update' is non-nil, it will be recomputed"
          (address (car (remove-if-not #'sockets:inet-address-private-p addresses))))
     (and address
          (setf get-local-address address))))
+
+(defmethod make-local-sock (context type)
+  "Make a locally bound socket of type `type' within `context' using the `transport'
+type. Returns two values: the socket created and the address that was bound to in `zmq:connect' format"
+  (let ((sock (zmq:socket context type))
+        (addr (format nil "tcp://~A:~A" (get-local-address :update t :as :string) (+ 50000 (random 10000)))))
+    ;; TODO: ^^ Port generation sucks. Bind and try :O
+    (log-for (warn) "TODO: Local sock on ~A" addr)
+    (or (handler-case (prog1 nil (zmq:bind sock addr))
+          (simple-error ()
+            (log-for (warn) "Failed to bind on ~A. Trying again." addr)
+            (zmq:close sock)
+            (make-local-sock context type)))
+
+        (values sock addr))))
+

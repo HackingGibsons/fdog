613814281bba99134f669b77fc50ff22867820cf
Reed Lipman <rmlipman@gmail.com>
2011-10-10 16:45:44 -0400

    Extract (string-upcase (concatentate... to a function

--- src/control/app/controllers/api.lisp
+++ src/control/app/controllers/api.lisp
@@ -7,32 +7,33 @@
 
 (define-condition http-error-condition () ())
 
-;;; TODO: make these (intern (string-upcase ... function calls
 (defmacro def-http-code (code desc &key default)
   "Macro for functionality related to HTTP status codes. Currently creates a condition and API handler.
 'code' - the numerical status code (404, 500...)
 'desc' - the description of the status code (\"Not found\", \"Internal server error\")
 'default' - A format string for a default error message to be encoded in the JSON"
 
-  `(progn
-     (defmethod handle-http-condition ((condition ,(intern (string-upcase (concatenate 'string (write-to-string code) "-condition")))) handler request raw)
-       (declare (ignorable raw))
-       (with-slots (data) condition
-         (with-chunked-stream-reply (handler request stream
-                                     :code ,code :status ,desc
-                                     :headers ((header-json-type)))
-           (log-for (trace) "[Condition] ~A" condition)
-           ,(if default
-              `(json:encode-json `((:error . ,(format nil ,@default))) stream)
-              `(json:encode-json `((:error . ,(format nil "~A" data))) stream)))))
+  (flet ((http-symbol (&rest args)
+           (string-upcase (format nil "~{~A~}" args))))
+    `(progn
+       (defmethod handle-http-condition ((condition ,(intern (http-symbol code "-condition"))) handler request raw)
+         (declare (ignorable raw))
+         (with-slots (data) condition
+           (with-chunked-stream-reply (handler request stream
+                                       :code ,code :status ,desc
+                                       :headers ((header-json-type)))
+             (log-for (trace) "[Condition] ~A" condition)
+             ,(if default
+                `(json:encode-json `((:error . ,(format nil ,@default))) stream)
+                `(json:encode-json `((:error . ,(format nil "~A" data))) stream)))))
 
-     (define-condition ,(intern (string-upcase (concatenate 'string (write-to-string code) "-condition"))) (http-error-condition)
-       ((data :initform ,desc
-              :initarg :data
-              :reader ,(intern (string-upcase (concatenate 'string (write-to-string code) "-data")))))
-       (:report (lambda (c s)
-                  (format s ,(string-upcase (concatenate 'string (write-to-string code) " Raised: ~A")) (,(intern (string-upcase (concatenate 'string (write-to-string code) "-data"))) c)))))
-     (export (find-symbol ,(string-upcase (concatenate 'string (write-to-string code) "-condition")) ':fdog-control) ':fdog-control)))
+       (define-condition ,(intern (http-symbol code "-condition")) (http-error-condition)
+         ((data :initform ,desc
+                :initarg :data
+                :reader ,(intern (http-symbol code "-data"))))
+         (:report (lambda (c s)
+                    (format s ,(http-symbol code " Raised: ~A") (,(intern (http-symbol code "-data")) c)))))
+       (export (find-symbol ,(http-symbol code "-condition") ':fdog-control) ':fdog-control))))
 
 (def-http-code 400 "Bad request")
 (def-http-code 404 "Not found" :default ("Endpoint ~A not found." (api-subpath request)))

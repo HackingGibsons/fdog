40fc7df3d9dde14672f385780d6da3dc214d083b
Yaroslav Shirokov <slava@hackinggibsons.com>
2011-10-11 22:40:46 -0400

    [afdog] Configurable number of beats to keep archived

--- src/agent/classes/standard-organ.lisp
+++ src/agent/classes/standard-organ.lisp
@@ -13,12 +13,17 @@
                   :accessor organ-outgoing-sock)))
 
 (defclass standard-beating-organ (standard-organ)
-  ((last-beat :initform '(nil nil)
-              :accessor last-beat)
+  ((keep-beats :initform 5
+               :accessor keep-beats)
+   (last-beat :accessor last-beat)
    (tag :initform :anonymous-organ
         :initarg :tag
         :accessor organ-tag
         :documentation "A beating organ heads it's heartbeat reply with a keyword from this slot.")))
+
+(defmethod initialize-instance :after ((organ standard-beating-organ) &key)
+  "Pad the `last-beat' list with nils"
+  (setf (last-beat organ) (make-list (keep-beats organ) :initial-element nil)))
 
 (defmethod organ-tag ((organ t))
   "An untagged `organ' should look like it's type. Makes mapping across them easier."

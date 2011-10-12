7d600c9a006179776bd43e77c40d4fe5f8c81b57
Yaroslav Shirokov <slava@hackinggibsons.com>
2011-10-11 21:20:20 -0400

    [afdog-io-callbacks] Be able to just use format t rather than having to run the logger.

--- utils/listener.lisp
+++ utils/listener.lisp
@@ -1,6 +1,6 @@
 (in-package :afdog-utils)
 
-(defun zmq-connect-and-dump (socktype addr &rest keys &key &allow-other-keys)
+(defun zmq-connect-and-dump (socktype addr &rest keys &key (use-log t) &allow-other-keys)
   (zmq:with-context (ctx 1)
     (zmq:with-socket (sock ctx socktype)
       (zmq:connect sock addr)
@@ -9,9 +9,14 @@
         (zmq:setsockopt sock zmq:subscribe (getf keys :subscribe)))
 
       (let ((msg (make-instance 'zmq:msg)))
-        (log-for (trace) "Starting message read...")
+        (if use-log
+            (log-for (trace) "Starting message read...")
+            (format t "Starting message read...~%"))
         (loop while (zmq:recv! sock msg) do
-             (log-for (trace) "Message(~A): [~A]" (zmq:msg-size msg) (zmq:msg-data-as-string msg)))))))
+             (if use-log
+                 (log-for (trace) "Message(~A): [~A]" (zmq:msg-size msg) (zmq:msg-data-as-string msg))
+                 (format t "Message(~A): [~A]~%" (zmq:msg-size msg) (zmq:msg-data-as-string msg))))))))
+
 
 
 

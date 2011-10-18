7b4b951d91b1ca28e66314e31a794b94d5381b4a
Yaroslav Shirokov <slava@hackinggibsons.com>
2011-10-18 18:27:26 -0400

    [nst] FIXTURES! FIXTURES!

--- tests/agent.lisp
+++ tests/agent.lisp
@@ -1,27 +1,17 @@
 (in-package :afdog-tests)
 
-(defclass test-agent (agent::standard-agent) ())
+;; Test structures
+(defclass test-agent (agent::standard-agent)
+  ()
+  (:documentation "A `standard-agent' derivative we can insert probes into to test things."))
 
-(defun make-test-agent ()
-  (make-instance 'test-agent))
+;; Test fixtures
+(def-fixtures with-agent-and-runner ()
+  (agent (make-instance 'test-agent))
+  (agent-runner (lambda () (run-agent agent))))
 
-(defun make-test-runner ()
-  (let ((test-agent (make-test-agent)))
-    (flet ((test-thread ()
-             (agent::run-agent test-agent)))
-      (values (lambda () (bt:make-thread #'test-thread :name "test-thread")) test-agent))))
-
+;; TODO: Garbage
 (def-test (can-test-nothing :group basic-tests)
-    (:true)
+    :true
   t)
 
-(def-test (events-not-zero :group basic-tests)
-    (:true)
-  (multiple-value-bind (runner agent) (make-test-runner)
-    (let ((thread (funcall runner)))
-      (sleep 0.1)
-      (bt:destroy-thread thread)
-      (describe agent)
-      (not (zerop (agent::agent-event-count agent))))))
-
-    

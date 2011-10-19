9c23a4f282c6c41ad2a43f7a625c26ee8727ce24
Yaroslav Shirokov <slava@hackinggibsons.com>
2011-10-18 22:25:19 -0400

    [nst] Move the boot event specialization closer to the reason it's specialized at all

--- tests/agent.lisp
+++ tests/agent.lisp
@@ -5,11 +5,6 @@
   ()
   (:documentation "A `standard-agent' derivative we can insert probes into to test things."))
 
-(defmethod agent-special-event :after ((agent test-agent) (head (eql :boot)) event)
-  "Boot event for the test agent"
-  (format t "Agent is booting.~%")
-  (nst:nst-cmd :run-group booted-agent-tests))
-
 ;; Test cases
 (def-test (can-test-nothing :group basic-tests) :true
   t)
@@ -20,6 +15,11 @@
   agent)
 
 ;; Boots up the running agent tests
+(defmethod agent-special-event :after ((agent test-agent) (head (eql :boot)) event)
+  "Boot event for the test agent, runs the `booted-agent-tests' group"
+  (format t "Agent is booting.~%")
+  (nst:nst-cmd :run-group booted-agent-tests))
+
 (def-test (test-running-agent :group basic-tests :fixtures (agent-fixture))
     (:process (:eval (handler-case (bt:with-timeout (1)
                                      (run-agent agent))

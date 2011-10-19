1bfa815b8dc5698874908a1276c9d6bbc4c4e782
Yaroslav Shirokov <slava@hackinggibsons.com>
2011-10-19 00:25:58 -0400

    [fix-vendored-libs] Working on externals

--- src/make/00-sys.mk
+++ src/make/00-sys.mk
@@ -5,10 +5,16 @@ VERBOSITY ?= 2 # [0, 3]
 # Helper
 SHUTUP = > /dev/null 2> /dev/null
 
-init: sanity-check quicklisp asdf
+init: sanity-check quicklisp asdf externals
 	@echo "=> Vendor init"
 	$(MAKE) -C $(ROOT)/vendor init
 
 
 sanity-check: $(ROOT)/$(TARGET).asd $(LISP) $(QL_SETUP)
 	@echo "!> Environment looks sane. I'll allow this."
+
+externals:
+	$(MAKE) -C $(ROOT)/vendor all
+
+externals-clean:
+	$(MAKE) -C $(ROOT)/vendor clean
--- src/make/99-clean.mk
+++ src/make/99-clean.mk
@@ -1,2 +1,2 @@
 clean-all: | clean
-clean: | lisp-fasl-clean asdf-clean lisp-clean
+clean: | lisp-fasl-clean asdf-clean lisp-clean externals-clean

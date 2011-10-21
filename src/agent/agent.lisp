e2c75753fbcb5132f50b37ca88314c7ead65a376
Yaroslav Shirokov <slava@hackinggibsons.com>
2011-10-21 00:12:02 -0400

    [afdog-launcher] Replicated the base (make-agent ..) operation in the launcher scaffold.

--- src/agent/agent.lisp
+++ src/agent/agent.lisp
@@ -1,7 +1,53 @@
 (in-package :agent)
 
-;; Makers
-(defun make-agent ()
-  "Agent maker wrapper"
-  (let* ((agent (make-instance 'standard-agent)))
-    agent))
+;; Base agent runner
+(defclass agent-runner ()
+  ((agent :initform nil :initarg :agent
+          :reader agent-instance
+          :documentation "Agent instance")
+   (handle :initform nil
+           :accessor agent-handle
+           :documentation "Handle to whatever ends up running the agent. Should be non-nil during operation."))
+  (:documentation "Base class for agent runners, only tracks an instance and abstract `handle'"))
+
+(defgeneric make-runner (style &key)
+  (:documentation "Make an agent running wrapper of the desired `style'.
+Style specializations should (call-next-method) to get a base isntance, then (change-class ..) the
+result into the desired type.")
+
+  (:method (style &rest initargs &key (class 'standard-agent) &allow-other-keys)
+    "Make the basic instance that is upgraded by other makers."
+    (let ((agent (apply #'make-instance class :allow-other-keys t initargs)))
+      (make-instance 'agent-runner :agent agent))))
+
+(defgeneric start (runner)
+  (:documentation "Start the `runner' by invoking `run-agent' on the enclosed agent"))
+(defgeneric stop (runner)
+  (:documentation "Stop the running agent in the container."))
+(defgeneric running-p (runner)
+  (:documentation "Predicate of if the given runner is currently running an agent.")
+  (:method ((runner agent-runner))
+    "Default running state is the presense of the handle."
+    (agent-handle runner)))
+
+(defclass thread-runner (agent-runner)
+  ())
+
+(defclass proc-runner (agent-runner)
+  ())
+
+;;
+;; Blocked runner, for operation within the current thread.
+;;
+(defclass blocked-runner (agent-runner)
+  ())
+
+(defmethod make-runner ((style (eql :blocked)) &key)
+  (let ((runner (call-next-method)))
+    (change-class runner 'blocked-runner)))
+
+(defmethod start ((runner blocked-runner))
+  (prog1 runner
+    (setf (agent-handle runner) (agent-instance runner))
+    (unwind-protect (run-agent (agent-handle runner))
+      (setf (agent-handle runner) nil))))

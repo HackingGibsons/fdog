ddd1ec291c6b325462a074bcb435a9236b342622
Sean Bryant <sean@vitrue.com>
2011-10-23 00:15:09 -0400

    [im-watching-you] stub out watch event

--- src/agent/behavior/behaviors/vision.lisp
+++ src/agent/behavior/behaviors/vision.lisp
@@ -5,3 +5,22 @@
   (let* ((thing (getf event :at)))
     (log-for (trace organ) "calling see on ~A with subject: ~A and info: ~A" organ (first thing) (rest thing))
     (apply #'see organ (first thing) thing)))
+
+(defclass watch-mixin ()
+  ())
+
+(defbehavior watch-when-told (:or ((:on (:command :watch :from :head))
+                                   (:on (:command :stop-watching :from :head)))
+                                  :include (watch-mixin)
+                                  :do :invoke-with-event) (organ event)
+  (log-for (trace organ) "organ: ~A event: ~A watch event" organ event)
+  ; (:HEAD :COMMAND :COMMAND :WATCH :WATCH (:PROCESS :PID :PID PID))
+  (case (getf event :command)
+    (:watch (watch behavior (getf event :watch)))
+    (:stop-watching (stop-watching behavior (getf event :stop-watching)))))
+
+(defmethod watch ((behavior watch-mixin) subject)
+  :TODO)
+
+(defmethod stop-watching ((behavior watch-mixin) subject)
+  :TODO)

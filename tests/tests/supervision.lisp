c2db5c31dd9e1306da0e4d6433ff82f210664452
Yaroslav Shirokov <slava@hackinggibsons.com>
2011-10-24 18:25:17 -0400

    [afdog-supervisor] Speaking test written with conversation helper.

--- tests/tests/supervision.lisp
+++ tests/tests/supervision.lisp
@@ -2,3 +2,7 @@
 
 (def-test (hyperrunner-is-running :group supervision-tests) :true
   (agent::running-p agent-runner))
+
+(def-test (hypervisor-is-speaking :group supervision-tests) :true
+  (with-agent-conversation (mouth-sock ear-sock) agent-uuid
+    (agent::parse-message (agent::read-message mouth-sock))))

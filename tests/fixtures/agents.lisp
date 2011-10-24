a6c8592aed170f3be2c09ca2bcb4ce8eb8f30749
Yaroslav Shirokov <slava@hackinggibsons.com>
2011-10-24 17:26:56 -0400

    [afdog-supervisor] Adding a hypervisor test agent that we can use to test against

--- tests/fixtures/agents.lisp
+++ tests/fixtures/agents.lisp
@@ -9,6 +9,14 @@
   ()
   (:documentation "A `standard-agent' derivative we can insert probes into to test things."))
 
+(defclass hypervisor-test-agent (standard-hypervisor-agent)
+  ()
+  (:documentation "A `standard-hypervisor-agent' that I can instrument."))
+
+(defmethod agent-special-event :after ((agent hypervisor-test-agent) (event-head (eql :boot)) event)
+  ;; Boot the hypervisor and make it loud
+  (make-announce-what-i-see (agent::find-organ agent :head)))
+
 (defmethod agent-special-event :after ((agent runner-agent) (head (eql :boot)) event)
   (make-speak-test-message (agent::find-organ agent :head))
 

2115d5dd2cbbb03976c2c517d29ce5b1527c790f
Yaroslav Shirokov <slava@hackinggibsons.com>
2011-10-26 17:26:51 -0400

    [afdog-spawn-scaffold] Changing the echo message.

--- tests/fixtures/behaviors/triggers.lisp
+++ tests/fixtures/behaviors/triggers.lisp
@@ -37,7 +37,7 @@
         (uuid (format nil "~A" (uuid:make-v4-uuid))))
     (when (equalp message '(:spawn :child))
       (send-message organ :command `(:command :speak
-                                                     :say (:todo :spawn :child ,uuid)))
+                                                     :say (:echo :spawn :child ,uuid)))
       (send-message organ :command `(:command :link
                                                      :link :agent
                                                      :agent (:uuid ,uuid :class leaf-test-agent :package :afdog-tests))))))

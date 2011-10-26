6c5a8ff2122b9383eb479c9dc4727df22ca32e37
Yaroslav Shirokov <slava@hackinggibsons.com>
2011-10-25 20:34:04 -0400

    [afdog-supervisor] Better insurance that the results dir gets created..

--- tests/package.lisp
+++ tests/package.lisp
@@ -13,10 +13,12 @@
 (defun run-all ()
   (let ((results-dir (merge-pathnames (make-pathname :directory '(:relative "tests" "results"))
                                       (asdf:system-source-directory :afdog-tests))))
+
+    (format t "Storing junit in ~A~%" results-dir)
+    (ensure-directories-exist results-dir :verbose t)
+
     (nst-cmd :run-group all-tests)
 
-    (log-for (trace) "Storing junit in ~A" results-dir)
-    (ensure-directories-exist results-dir :verbose t)
     (junit-results-by-group :dir results-dir
                             :if-file-exists :supersede
                             :if-dir-does-not-exist :create)))

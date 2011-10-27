2007a8bdfa97c0a9108147de6613fafb9524c609
Sean Bryant <sean@vitrue.com>
2011-10-27 16:32:26 -0400

    [afdog-crafty-hands] not an inversion test.

--- tests/fixtures/behaviors/triggers.lisp
+++ tests/fixtures/behaviors/triggers.lisp
@@ -60,6 +60,6 @@
                             :agent (:uuid ,(getf message :uuid)
                                           :class leaf-test-agent
                                           :package :afdog-tests))))
-    (unless (and (> (length message) 3) 
-                 (equalp (subseq message 0 3) '(:make :agent :uuid)))
+    (when (and (> (length message) 3) 
+               (equalp (subseq message 0 3) '(:make :agent :uuid)))
       (agent::send-message organ :command command))))

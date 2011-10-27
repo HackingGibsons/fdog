c1269a50554a4956e19643654d73468f9b763fd7
Sean Bryant <sean@vitrue.com>
2011-10-27 13:30:08 -0400

    [afdog-crafty-hands] making now emits an event of what it made so interested partys can do whatevers.

--- src/agent/behavior/behaviors/manipulation.lisp
+++ src/agent/behavior/behaviors/manipulation.lisp
@@ -5,8 +5,9 @@
   ;;  :command :make
   ;;  :make :agent
   ;;  :agent (:uuid uuid :class standard-leaf-agent :package :agent)))
-  (let ((what (get event :make)))
-    (make-item behavior what (getf event what))))
+  (let ((what (getf event :make)))
+    (make-item behavior what (getf event what))
+    (send-message organ :made `(:made ,what ,what ,(getf event what)))))
 
 (defgeneric make-item (behavior what info)
   (:documentation "Generic protocol for construction of things.")
@@ -36,8 +37,10 @@
     (when (and class package)
       (let* ((initargs `(:uuid ,uuid
                          :parent-uuid ,(agent-uuid (organ-agent (behavior-organ behavior)))
-                         :parent-mouth ,(mouth-addr (find-organ (organ-agent (behavior-organ behavior)) :mouth))))
-             (runner (apply #'make-runner *spawner* :class (class-name class) initargs)))
+                         :parent-mouth ,(mouth-addr (find-organ (organ-agent (behavior-organ behavior)) :mouth)))))
+             (format t "*spawner* => ~A" *spawner*)
+
+             (runner (apply #'make-runner *spawner* :class (class-name class) initargs))
 
         (and runner
              (start runner))))))

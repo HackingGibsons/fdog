b6d52020234328bccd3e18f9cea8e21b28328d3e
Yaroslav Shirokov <slava@hackinggibsons.com>
2011-11-02 18:13:45 -0400

    [afdog-agent-kill] Failing test

--- tests/tests/behavior.lisp
+++ tests/tests/behavior.lisp
@@ -163,6 +163,16 @@
     (zmq:send! e (agent::prepare-message `(:make :process :transaction-id ,transaction-id)))
     (do ((msg (agent::parse-message (agent::read-message m))
               (agent::parse-message (agent::read-message m))))
-        ((and (equalp (getf msg :made) :process) 
+        ((and (equalp (getf msg :made) :process)
               (equal (getf msg :transaction-id) transaction-id)) t))))
-    
+
+(def-test (agent-dies-when-asked :group basic-behavior-tests :fixtures (running-agent-fixture)) :true
+  (with-agent-conversation (m e) agent-uuid
+    (zmq:send! e (agent::prepare-message `(:kill :agent :agent ,agent-uuid)))
+    (do ((msg (agent::parse-message (agent::read-message m))
+              (agent::parse-message (agent::read-message m))))
+        ;; Expecting to hear: (:agent :death :agent ,agent-uuid)
+        ((and (>= (length msg) 4)
+              (equalp (subseq msg 0 2) '(:agent :death))
+              (equalp (getf msg :agent) agent-uuid))
+         :dead))))

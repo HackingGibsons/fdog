abca5755d9c52e1be1c4e915faafdd8cdc43cbc8
Yaroslav Shirokov <slava@hackinggibsons.com>
2011-11-04 22:02:20 -0400

    [afdog-cli-build] Wrapped commands in CLI handlers

--- src/cli/commands.lisp
+++ src/cli/commands.lisp
@@ -2,18 +2,24 @@
 
 (defcommand repl (argv)
   "Start a repl and nothing else"
-  #+sbcl
-  (sb-impl::toplevel-repl nil))
+  (with-cli-options (argv "Usage: repl [options]~%~@{~A~%~}~%")
+    nil ;; No option bindings
+    #+sbcl (sb-impl::toplevel-repl nil)))
 
 
 (defcommand help (argv &key (exit 0))
   "Show help"
   (if argv
-      (progn
+      (with-cli-options (argv "Usage: help [options] [command]~%~@{~A~%~}~%")
+        nil ;; No option bindings
         (format t "Help on command ~A:~%" (car argv))
-        (let ((doc (get-command (car argv) :doc)))
-          (when doc (format t "~A~%~%" doc)))
-        (funcall (get-command (car argv) :function) '("-h")))
+        (let ((doc (get-command (car argv) :doc))
+              (fun (get-command (car argv) :function)))
+          (if (and doc fun)
+              (ignore-errors
+                (format t "~A~%~%" doc)
+                (funcall (get-command (car argv) :function) '("-h")))
+              (format t "  Unknown command.~%"))))
       (progn
         (format t "Usage: ~A <command> [command-options]~%" *self*)
         (list-commands)))

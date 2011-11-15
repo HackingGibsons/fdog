69d23b3cbc0f35538763d43de1f7307f65c22e40
Yaroslav Shirokov <slava@hackinggibsons.com>
2011-11-14 21:50:37 -0500

    [afdog-cli-build] Every command must now handle -h correctly

--- tests/tests/cli.lisp
+++ tests/tests/cli.lisp
@@ -9,3 +9,16 @@
     (:apply length (:predicate (lambda (c) (> c 1))))
   afdog-cli::*commands*)
 
+
+;; This test is kinda sad, since all you have to do to pass it is not
+;; explode when passed -h..
+(def-test (each-command-responds-to-help :group cli-tests)
+    (:each (:apply
+            (lambda (x)
+              (if (equalp x "help")
+                  :help-would-exit
+                  (ignore-errors
+                    (prog1 :pass (funcall (get-command x :function) '("-h"))))))
+            :true))
+  (mapcar #'symbol-name (mapcar #'car afdog-cli::*commands*)))
+

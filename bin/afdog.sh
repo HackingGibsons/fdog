b05d3e4f500f0508665c3d1fa5e06a240ca3eee0
Yaroslav Shirokov <slava@hackinggibsons.com>
2011-11-15 18:02:56 -0500

    [afdog-linux-script] Merged too soon! It didn't work!

--- bin/afdog.sh
+++ bin/afdog.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 BASE=$(dirname $(greadlink -f $0 2> /dev/null || readlink -f $0 2> /dev/null))
 export AFDOG=$BASE/$(basename $0)
-/usr/bin/env sbcl --script $BASE/afdog.script
+/usr/bin/env sbcl --script $BASE/afdog.script $@

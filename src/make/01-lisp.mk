c905e9b5a6860760f4f1c9de31dfa8ee1d181a21
Yaroslav Shirokov <slava@hackinggibsons.com>
2011-11-17 12:48:09 -0500

    [afdog-fix-asdf] Splitting the CL_SOURCE_REGISTRY away from the lisp binary and putting it in front of buildapp as well

--- src/make/01-lisp.mk
+++ src/make/01-lisp.mk
@@ -1,6 +1,7 @@
 # Lithp thtuff
 LISP_BIN ?= $(shell which sbcl || echo /sbcl/does/not/exist)
-LISP ?= CL_SOURCE_REGISTRY='$(ROOT):$(ROOT)/vendor//' $(LISP_BIN)
+LISP_PREFIX ?= CL_SOURCE_REGISTRY='$(ROOT):$(ROOT)/vendor//'
+LISP ?= $(LISP_PREFIX) $(LISP_BIN)
 
 # Asdf configuration
 REGISTRYD ?= $(HOME)/.config/common-lisp/source-registry.conf.d

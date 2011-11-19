1b61a4d44bc0829c992320367b0462cefd04670c
Sean Bryant <sean@vitrue.com>
2011-11-19 01:36:50 -0500

    [afdog-mongrel2-control-agent] Test to see if the mongrel2-test-agent spawns a mongrel process. This isn't working yet :fire:

--- tests/tests/mongrel2.lisp
+++ tests/tests/mongrel2.lisp
@@ -1,1 +1,10 @@
 (in-package :afdog-tests)
+
+(def-test (mongrel2-agent-starts :group mongrel2-agent-tests :fixtures ()) :true
+  (with-agent-conversation (m e :timeout 60) mongrel2-uuid
+    (do ((server-pid (fdog-models:mongrel2-server-pid (fdog-models:servers :refresh t :one t))
+                     (fdog-models:mongrel2-server-pid (fdog-models:servers :refresh t :one t))))
+        (pid pid)
+      (when (ignore-errors (iolib.syscalls:kill server-pid 0))
+        (setf pid server-pid))
+      (sleep 0.1))))

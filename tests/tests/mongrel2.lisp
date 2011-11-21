c12b2c9d5c0d09fa8a5e77c1a2623301b3dd41bc
Sean Bryant <sean@vitrue.com>
2011-11-21 12:40:12 -0500

    [afdog-mongrel2-control-agent] Test restarting when SMDS (Sudden Mongrel Death Syndrome) hits. This test causes :fire:

--- tests/tests/mongrel2.lisp
+++ tests/tests/mongrel2.lisp
@@ -19,5 +19,37 @@
       (setf pid server-pid)
       running-p)))
 
+(def-test (mongrel2-agent-restarts :group mongrel2-agent-tests :fixtures (db-path-fixture mongrel2-agent-fixture)) :true
+  (progn
+    (with-agent-conversation (m e :timeout 60) mongrel2-uuid
+      (do ((msg (parse-message (read-message m))
+                (parse-message (read-message m))))
+          (msg))) ;; A message means the thing has booted.
+
+    (fdog-models:connect db-path)
+
+    (handler-bind ((bt:timeout #'(lambda (c)
+                                   (declare (ignorable c))
+                                   (error "Timing out")))) ;; nst likes generic errors more.
+
+      ;; TODO: Clean up this horrible copy-pasta. Turn into a fixture or helper. Why not both?
+      (let* ((server (fdog-models:servers :refresh t :one t))
+             (server-pid (and server (fdog-models:mongrel2-server-pid server)))
+             (running-p (fdog-models:mongrel2-server-running-p server)))
+        (setf pid server-pid)
+        running-p)
+
+      (iolib.syscalls:kill pid iolib.syscalls:sigkill)
+
+      (bt:with-timeout (10)
+        (do* ((server (fdog-models:servers :refresh t :one t)
+                      (fdog-models:servers :refresh t :one t))
+              (server-pid (and server (fdog-models:mongrel2-server-pid server))
+                          (and server (fdog-models:mongrel2-server-pid server)))
+              (running-p (and server (fdog-models:mongrel2-server-running-p server))
+                         (and server (fdog-models:mongrel2-server-running-p server))))
+             ((and running-p
+                   (not (= pid server-pid)))
+              (setf pid server-pid)))))))
 
 

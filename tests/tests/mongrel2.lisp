932b4f6737dba6add92ecebc9da9814c34dbc129
Sean Bryant <sean@vitrue.com>
2011-11-22 14:54:24 -0500

    [afdog-mongrel2-control-agent] A more better restart test! :star::star::star:

--- tests/tests/mongrel2.lisp
+++ tests/tests/mongrel2.lisp
@@ -91,16 +91,41 @@
               :saw-new-process)))))
 
 
-(def-test (mongrel2-agent-watches-existing-mongrel :group mongrel2-agent-tests :fixtures (db-path-fixture running-mongrel2-fixture mongrel2-agent-fixture))
-    (:seq (:eql :server-is-running)
-          (:eql :server-is-watched))
-  (list
-   (if (fdog-models:mongrel2-server-running-p server)
-       (and (setf pid (fdog-models:mongrel2-server-pid server))
-            :server-is-running)
-       :server-is-not-running)
+(def-test (mongrel2-agent-watches-and-restarts-existing-mongrel :group mongrel2-agent-tests :fixtures (db-path-fixture mongrel2-agent-fixture))
+    (:seq (:eql :first-process)
+          (:eql :runner-dead)
+          (:eql :same-server-found)
+          (:eql :mongrel2-dead)
+          (:eql :made-new-process)
+          (:eql :saw-new-process))
 
-   (when (fdog-models:mongrel2-server-running-p server)
+  (list
+   (with-agent-conversation (m e) mongrel2-uuid
+     (do* ((msg (parse-message (read-message m))
+                (parse-message (read-message m)))
+           (process nil (getf msg :process)))
+          ;; TODO: One of these can never match, it destructures the :made message stupid
+          ((or (and (eql (getf msg :saw) :process)
+                    (getf process :pid))
+               (and (eql (getf msg :made) :process)
+                    (getf msg :pid)))
+           (setf pid (or (getf process :pid) (getf msg :pid)))
+           :first-process))) ;; We have a server
+
+   (progn
+     (stop mongrel2-runner)
+     (unless (running-p mongrel2-runner)
+       :runner-dead))
+
+   (progn
+     (fdog-models:connect db-path)
+     (setf mongrel2-runner (make-runner :test :include '(:afdog-tests)
+                                        :class 'mongrel2-test-agent
+                                        :root *root* ;; different root for the test agents
+                                        :uuid mongrel2-uuid))
+
+     (start mongrel2-runner)
+
      (with-agent-conversation (m e :timeout 30) mongrel2-uuid
        (do* ((msg (parse-message (read-message m))
                   (parse-message (read-message m)))
@@ -115,4 +140,49 @@
                   process
                   running-p
                   (= pid saw-pid))
-             :server-is-watched))))))
+             :same-server-found))))
+
+   (progn
+     (let ((server (fdog-models:servers :name "control" :refresh t :one t)))
+       (fdog-models:mongrel2-server-signal/block server :stop)
+       (if (fdog-models:mongrel2-server-running-p server)
+           :mongrel2-running
+           :mongrel2-dead)))
+
+   ;; A new process has been made
+      (with-agent-conversation (m e :timeout 30) mongrel2-uuid
+        (fdog-models:connect db-path)
+        (do* ((msg (parse-message (read-message m))
+                   (parse-message (read-message m)))
+              (made (getf msg :made)
+                    (or made (getf msg :made)))
+              (made-info (getf msg made)
+                         (or made-info (getf msg made)))
+              (server (fdog-models:servers :name "control" :refresh t :one t)
+                      (fdog-models:servers :name "control" :refresh t :one t))
+              (server-pid (and server (fdog-models:mongrel2-server-pid server))
+                          (and server (fdog-models:mongrel2-server-pid server))))
+             ((and made-info server
+                   (fdog-models:mongrel2-server-running-p server)
+                   (equalp (fdog-models:mongrel2-server-pid server)
+                           (getf made-info :pid)))
+              :made-new-process)))
+
+
+      ;; And that process stuck around.
+      (with-agent-conversation (m e :timeout 30) mongrel2-uuid
+        (do* ((msg (parse-message (read-message m))
+                   (parse-message (read-message m)))
+              (saw nil (getf msg :saw))
+              (process nil (getf msg :process))
+              (saw-pid nil (getf process :pid))
+              (server (fdog-models:servers :name "control" :refresh t :one t)
+                      (fdog-models:servers :name "control" :refresh t :one t))
+              (running-p (fdog-models:mongrel2-server-running-p server)
+                         (fdog-models:mongrel2-server-running-p server)))
+             ((and (eql saw :process)
+                   process
+                   running-p
+                   (not (= pid saw-pid)))
+              (setf pid saw-pid)
+              :saw-new-process)))))

3a05393fa7c631daec2230963e96b990aa905fe1
Sean Bryant <sean@vitrue.com>
2011-11-22 13:00:30 -0500

    [afdog-mongrel2-control-agent] Export the helper functions used by the mongrel2 agent.

--- src/fdog/agents/packages.lisp
+++ src/fdog/agents/packages.lisp
@@ -5,4 +5,6 @@
         #:agent
         #:log5)
   (:export :mongrel2-agent
-           :rooted-agent-mixin))
+           :rooted-agent-mixin
+           :ensure-mongrel2-root-layout
+           :initialize-mongrel2-configuration))

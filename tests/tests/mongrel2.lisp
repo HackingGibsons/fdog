4f9c39613ddfee2046b5dff22b76b5fa44e99337
Yaroslav Shirokov <slava@hackinggibsons.com>
2011-11-21 23:47:30 -0500

    [afdog-linking-surgery] re-fixing the restart test.

--- tests/tests/mongrel2.lisp
+++ tests/tests/mongrel2.lisp
@@ -25,50 +25,50 @@
 (def-test (mongrel2-agent-restarts :group mongrel2-agent-tests :fixtures (db-path-fixture mongrel2-agent-fixture))
     (:seq (:eql :first-process)
           (:eql :not-running)
-          (:eql :saw-new-process)
-          (:eql :made-new-process))
+          (:eql :made-new-process)
+          (:eql :saw-new-process))
   (list
-    (with-agent-conversation (m e) mongrel2-uuid
-      (do* ((msg (parse-message (read-message m))
-                 (parse-message (read-message m)))
-            (process nil (getf msg :process)))
-           ((or (and (eql (getf msg :saw) :process)
-                     (getf process :pid))
-                (and (eql (getf msg :made) :process)
-                     (getf msg :pid)))
-            (format t "Msg ~A~%" msg)
-            :first-process))) ;; We have a server
-      (progn
-        (fdog-models:connect db-path)
+   (with-agent-conversation (m e) mongrel2-uuid
+     (do* ((msg (parse-message (read-message m))
+                (parse-message (read-message m)))
+           (process nil (getf msg :process)))
+          ((or (and (eql (getf msg :saw) :process)
+                    (getf process :pid))
+               (and (eql (getf msg :made) :process)
+                    (getf msg :pid)))
+           (format t "Msg ~A~%" msg)
+           :first-process))) ;; We have a server
 
-        ;; Kill the running server.
-        (let* ((server (fdog-models:servers :name "control" :refresh t :one t))
-               (server-pid (and server (fdog-models:mongrel2-server-pid server)))
-               (running-p (fdog-models:mongrel2-server-running-p server)))
-          (when running-p
-            (setf pid server-pid)
-            (fdog-models:mongrel2-server-signal/block server :stop)
-            (if (fdog-models:mongrel2-server-running-p server)
-                :running
-                :not-running))))
+   (progn
+     (fdog-models:connect db-path)
+     ;; Kill the running server.
+     (let* ((server (fdog-models:servers :name "control" :refresh t :one t))
+            (server-pid (and server (fdog-models:mongrel2-server-pid server)))
+            (running-p (fdog-models:mongrel2-server-running-p server)))
+       (when running-p
+         (setf pid server-pid)
+         (fdog-models:mongrel2-server-signal/block server :stop)
+         (if (fdog-models:mongrel2-server-running-p server)
+             :running
+             :not-running))))
 
       ;; A new process has been made
       (with-agent-conversation (m e :timeout 30) mongrel2-uuid
+        (fdog-models:connect db-path)
         (do* ((msg (parse-message (read-message m))
                    (parse-message (read-message m)))
-              (made nil (getf msg :made))
-              (made-pid (getf msg :pid))
+              (made (getf msg :made) (getf msg :made))
+              (made-info (getf msg made)
+                         (or made-info (getf msg made)))
               (server (fdog-models:servers :name "control" :refresh t :one t)
                       (fdog-models:servers :name "control" :refresh t :one t))
-              (running-p (fdog-models:mongrel2-server-running-p server)
-                         (fdog-models:mongrel2-server-running-p server)))
-             ((and (eql made :process)
-                   running-p
-                   (not (= pid made-pid)))
-              (format t "Got Made!~%")
-              :made-new-process)
-          (format t "Waiting for Made But Got: ~A~%" msg)))
-
+              (server-pid (and server (fdog-models:mongrel2-server-pid server))
+                          (and server (fdog-models:mongrel2-server-pid server))))
+             ((and made-info server
+                   (fdog-models:mongrel2-server-running-p server)
+                   (equalp (fdog-models:mongrel2-server-pid server)
+                           (getf made-info :pid)))
+              :made-new-process)))
 
       ;; And that process stuck around.
       (with-agent-conversation (m e :timeout 30) mongrel2-uuid

18f8c5d662cf3b4a17da99eeadc8a8dfbc72ad3b
Sean Bryant <sean@vitrue.com>
2011-12-01 14:14:32 -0500

    [afdog-dep-core] Compile a core of just our dependencies once. :cake:
    Then just use the core when we need to run tests.
    This saves us time from compiling heavy dependencies like ironclad and babel.

--- tests/make/00-test.mk
+++ tests/make/00-test.mk
@@ -1,29 +1,54 @@
+#-*- mode:makefile-gmake; -*-
+AFDOG_ASD ?= $(ROOT)/afdog.asd
+DEPS_PREFIX ?= afdog-$(shell (md5sum $(AFDOG_ASD) || md5 -r $(AFDOG_ASD)) | awk '{ print $$1; }')
+DEPS_EXT ?= .core
+DEPS_NAME ?= $(DEPS_PREFIX)$(DEPS_EXT)
+DEPS_DIR  ?= /tmp
+
 test: clean-all init
 	@echo "=> Running tests."
 	rm -rf $(ROOT)/sbcl.core
 	$(MAKE) run-tests-with-core
 	$(MAKE) clean
 
-$(ROOT)/sbcl.core:
+$(ROOT)/sbcl.core: $(DEPS_DIR)/$(DEPS_NAME)
 	echo "=> Compiling test core."
 	LD_LIBRARY_PATH=$$LD_LIBRARY_PATH:$(ROOT)/vendor/libfixposix/build/lib \
 	CPATH=$(ROOT)/vendor/libfixposix/src/include \
-	$(LISP)  --script $(ROOT)/bin/afdog.script \
-	                  repl \
-                      '(ql:quickload :afdog-tests)' \
-                      '(sb-ext:save-lisp-and-die "sbcl.core")'
+	$(DEPS_DIR)/$(DEPS_NAME) \
+		--noprint \
+		--eval '(defparameter *afdog-system* (asdf:find-system :afdog))' \
+		--eval '(defparameter *afdog-test-system* (asdf:find-system :afdog-tests))' \
+		--eval '(setf (ASDF::COMPONENT-LOAD-DEPENDENCIES *afdog-system*) nil)' \
+		--eval '(setf (ASDF::COMPONENT-LOAD-DEPENDENCIES *afdog-test-system*) nil)' \
+		--eval '(asdf:disable-output-translations)' \
+		--eval "(asdf:load-system *afdog-system*)" \
+		--eval "(asdf:load-system *afdog-test-system*)" \
+		--eval '(sb-ext:save-lisp-and-die "sbcl.core")'
 
 run-tests-with-core: $(ROOT)/sbcl.core
 	@echo "=> Making sure we don't use a stale core"
 	SBCL_HOME=$(ROOT) $(MAKE) run-tests
 	rm -rf $(ROOT)/sbcl.core
 
+$(DEPS_DIR)/$(DEPS_NAME):
+	@echo "=> Cleaning up old cores."
+	rm -rf $(DEPS_DIR)/afdog-*$(DEPS_EXT)
+	@echo "=> Compiling dependince core: $(DEPS_DIR)/$(DEPS_NAME)"
+	LD_LIBRARY_PATH=$$LD_LIBRARY_PATH:$(ROOT)/vendor/libfixposix/build/lib \
+	CPATH=$(ROOT)/vendor/libfixposix/src/include \
+	$(LISP) --noinform --disable-ldb --lose-on-corruption --end-runtime-options \
+		--disable-debugger \
+		--noprint \
+		--load $(QL_ROOT_PATH)/setup.lisp \
+		--eval "(mapc #'ql:quickload (cdar (asdf:component-depends-on 'asdf:compile-op (asdf:find-system :afdog))))" \
+		--eval "(mapc #'ql:quickload '(#:nst))" \
+		--eval '(sb-ext:save-lisp-and-die "$(DEPS_DIR)/$(DEPS_NAME)" :executable t)'
+
 run-tests:
 	LD_LIBRARY_PATH=$$LD_LIBRARY_PATH:$(ROOT)/vendor/libfixposix/build/lib \
 	CPATH=$(ROOT)/vendor/libfixposix/src/include \
 	$(LISP) --load $(QL_ROOT_PATH)/setup.lisp \
-			--no-userinit \
-			--eval '(ql:quickload :afdog-tests)' \
-			--eval '(asdf:test-system :afdog)' \
-			--eval '(quit)'
+		--eval '(asdf:test-system :afdog)' \
+		--eval '(quit)'
 	rm -rf $(ROOT)/sbcl.core

63a2d519e214d08c316d9fa1141f9656ae3153c9
Reed Lipman <rmlipman@gmail.com>
2011-12-12 12:51:40 -0500

    request-queue-length should use endpoint-queue-counter
    not endpoint-queue-key

--- src/services/forwarder/methods/queue.endpoint.lisp
+++ src/services/forwarder/methods/queue.endpoint.lisp
@@ -79,7 +79,7 @@
 (defmethod request-queue-length ((forwarder fdog-forwarder))
   (redis:with-named-connection (redis :host *redis-host*
                                       :port *redis-port*)
-   (parse-integer (or (redis:lred-hget redis (endpoint-queue-key forwarder) :count) "0"))))
+   (parse-integer (or (redis:lred-hget redis (endpoint-queue-counter forwarder) :count) "0"))))
 
 (defun total-request-queue-length ()
   (redis:with-named-connection (redis :host *redis-host*

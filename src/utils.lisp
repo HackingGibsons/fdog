9931b69f8fbe76e7fccf65c4479ca31b05060a54
Reed Lipman <rmlipman@gmail.com>
2011-12-12 10:42:04 -0500

    Change asdf: to use merge-pathnames

--- src/utils.lisp
+++ src/utils.lisp
@@ -103,7 +103,7 @@ Performs kill, sleep, then kill -9 to catch stragglers, then deletes the pidfile
            (delete-files (directory)
              (cl-fad:walk-directory directory
                                     (lambda (file) (delete-file file)))))
-    (let* ((run-directory (asdf:system-relative-pathname :afdog "run/")))
+    (let* ((run-directory (merge-pathnames "run/" *root*)))
       (log-for (info) "Killing all spawned processes with kill -9")
       (kill-files run-directory iolib.syscalls:sigkill)
 

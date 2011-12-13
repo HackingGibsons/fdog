6613f9df1d4a6a9093af481636a179ca6fe38eb6
Reed Lipman <rmlipman@gmail.com>
2011-12-13 11:06:17 -0500

    Add make kill-everything-fast task to just kill -9 (no sleeping)

--- src/make/99-clean.mk
+++ src/make/99-clean.mk
@@ -1,13 +1,20 @@
 clean-all: | clean
 clean: | lisp-fasl-clean asdf-clean lisp-clean externals-clean clean-build clean-all-bundle
 
+.PHONY: kill-everything-clean
+kill-everything-fast:
+	KILL_EVERYTHING_FAST=1 $(MAKE) kill-everything
+
 .PHONY: kill-everything
 .IGNORE: kill-everything
 kill-everything:
+ifeq ($(KILL_EVERYTHING_FAST),1)
+else
 	@echo "=> Killing all processes"
 	cat $(ROOT)/run/* | xargs kill
 	@echo "=> Waiting for processes to die"
 	sleep 10
+endif
 	@echo "=> Kill -9'ing stragglers"
 	cat $(ROOT)/run/* | xargs kill -9
 	@echo "=> Deleting stale pidfiles"

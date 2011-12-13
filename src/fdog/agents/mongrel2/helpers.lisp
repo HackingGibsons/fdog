dfe19b1d63d5502ab5d53ed2a46afab8b9ad6094
Yaroslav Shirokov <slava@hackinggibsons.com>
2011-12-13 15:48:24 -0500

    [afdog-mongrel2-three] Reload the server if it's running during a link and a helper to link every known server

--- src/fdog/agents/mongrel2/helpers.lisp
+++ src/fdog/agents/mongrel2/helpers.lisp
@@ -10,6 +10,17 @@
 
       (log-for (mongrel2-agent trace) "Found mongrel2 server ~A to link with make arguments of ~A." server arguments)
 
+      (when (fdog-models:mongrel2-server-running-p server)
+        (log-for (mongrel2-agent trace) "Reloading server: ~A" server)
+        (fdog-models:mongrel2-server-signal server :reload)
+        (log-for (mongrel2-agent trace) "Reloaded server: ~A" server))
+
       (send-message organ :command `(:command :link
                                               :link :process
-                                              :process (:pid ,pid ,@arguments))))))
+                                              :process (:pid ,pid ,@arguments)))
+      server)))
+
+(defmethod link-all-servers ((organ agent::standard-organ) config)
+  "Call (link-server) on every server"
+  (mapcar #'(lambda (s) (link-server organ s config))
+          (fdog-models:servers :refresh t)))

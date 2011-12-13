56740e0a820b0125faf26f344491e4764b452b96
Yaroslav Shirokov <slava@hackinggibsons.com>
2011-12-13 14:36:38 -0500

    [afdog-mongrel2-three] Testing that a newly created server starts

--- tests/tests/mongrel2.lisp
+++ tests/tests/mongrel2.lisp
@@ -304,3 +304,50 @@
             (hosts (and server (fdog-models:mongrel2-server-hosts server))))
        (and (find "api.example.com" hosts :test #'equalp :key #'fdog-models:mongrel2-host-name)
             :find-new-host)))))
+
+(def-test (mongrel2-agent-new-server-starts :group mongrel2-agent-tests :fixtures (db-path-fixture mongrel2-agent-fixture))
+    (:seq (:eql :need-filled)
+          (:eql :find-new-host)
+          (:eql :server-running))
+  (list
+   (with-agent-conversation (m e) mongrel2-uuid
+     (zmq:send! e (prepare-message
+                   `(:agent :need
+                            :need  :server
+                            :server (:name "forwarder" :port 7171 :hosts ("awesome.example.com")))))
+     (do* ((msg (parse-message (read-message m))
+                (parse-message (read-message m)))
+           (filled (and (equalp (car msg) :filled) msg)
+                   (or filled
+                       (and (equalp (car msg) :filled) msg))))
+          ((and filled
+                (getf filled :server))
+           (log-for (trace mongrel2-agent::agent-needs) "Filled: ~A" msg)
+           :need-filled)))
+
+   (progn
+     (ignore-errors (fdog-models:disconnect))
+     (ignore-errors (clsql:disconnect))
+     (fdog-models:connect db-path)
+     (let* ((server (fdog-models:servers :name "forwarder" :refresh t :one t))
+            (hosts (and server (fdog-models:mongrel2-server-hosts server))))
+       (and (find "awesome.example.com" hosts :test #'equalp :key #'fdog-models:mongrel2-host-name)
+            :find-new-host)))
+
+   (with-agent-conversation (m e) mongrel2-uuid
+     (ignore-errors (fdog-models:disconnect))
+     (ignore-errors (clsql:disconnect))
+     (fdog-models:connect db-path)
+
+     (do* ((msg (parse-message (read-message m))
+                (parse-message (read-message m)))
+           (process (getf msg :process)
+                    (getf msg :process))
+           (server (fdog-models:servers :name "forwarder" :refresh t :one t)
+                   (fdog-models:servers :name "forwarder" :refresh t :one t)))
+          ((and server (getf process :pid)
+                (equalp (getf process :pid)
+                        (fdog-models:mongrel2-server-pid server)))
+           :server-running)))))
+
+

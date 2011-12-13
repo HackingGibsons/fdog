c55a070c68d5ed7949dfef9efc7a33efe9c461e1
Yaroslav Shirokov <slava@hackinggibsons.com>
2011-12-13 14:17:02 -0500

    [afdog-mongrel2-three] Testing that the requested server is made.

--- tests/tests/mongrel2.lisp
+++ tests/tests/mongrel2.lisp
@@ -276,3 +276,29 @@
             (hosts (and server (fdog-models:mongrel2-server-hosts server))))
        (and (find "api.example.com" hosts :test #'equalp :key #'fdog-models:mongrel2-host-name)
             :find-new-host)))))
+
+(def-test (mongrel2-agent-creates-new-server :group mongrel2-agent-tests :fixtures (db-path-fixture mongrel2-agent-fixture))
+    (:seq (:eql :need-filled)
+          (:eql :find-new-host))
+  (list
+   (with-agent-conversation (m e) mongrel2-uuid
+     (zmq:send! e (prepare-message
+                   `(:agent :need
+                     :need  :server
+                     :server (:name "forwarder" :port 6969 :hosts ("api.example.com")))))
+     (do* ((msg (parse-message (read-message m))
+                (parse-message (read-message m)))
+           (filled (and (equalp (car msg) :filled) msg)
+                   (or filled
+                       (and (equalp (car msg) :filled) msg))))
+          ((and filled
+                (getf filled :server))
+           (log-for (trace mongrel2-agent::agent-needs) "Filled: ~A" msg)
+           :need-filled)))
+
+   (progn
+     (fdog-models:connect db-path)
+     (let* ((server (fdog-models:servers :name "forwarder" :refresh t :one t))
+            (hosts (and server (fdog-models:mongrel2-server-hosts server))))
+       (and (find "api.example.com" hosts :test #'equalp :key #'fdog-models:mongrel2-host-name)
+            :find-new-host)))))

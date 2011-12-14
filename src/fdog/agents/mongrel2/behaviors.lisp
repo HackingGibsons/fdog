ce808ddcfd36b4d161a5494cf86be687afa83caa
Yaroslav Shirokov <slava@hackinggibsons.com>
2011-12-14 18:11:31 -0500

    [afdog-mongrel2-remove-host] Also delete server.

--- src/fdog/agents/mongrel2/behaviors.lisp
+++ src/fdog/agents/mongrel2/behaviors.lisp
@@ -57,7 +57,8 @@
           (send-message organ :command `(:command :speak
                                           :say (:filled :need
                                                 :need ,what
-                                                ,what ,(mapcar #'server-info-cons servers)))))))))
+                                                ,what ,(mapcar #'server-info-cons servers))))
+          (clsql:delete-instance-records (list server)))))))
 
 (defmethod agent-needs ((agent mongrel2-agent) (organ agent-head) (what (eql :server)) need-info)
   "A :need for a :server gets filled when heard."

2646669881c6c1a56c6ec4c82992bef3f14821df
Yaroslav Shirokov <slava@hackinggibsons.com>
2012-01-05 19:35:30 -0500

    [afdog-mongrel2-handler-service] Testing the advertisement of handlers.

--- tests/tests/mongrel2.lisp
+++ tests/tests/mongrel2.lisp
@@ -828,3 +828,57 @@
           (prog1 :handler-has-endpoint
             (log-for (trace mongrel2-agent::agent-needs) "Target: ~A" target))))))
 
+(def-test (mongrel2-agent-announces-handler :group mongrel2-agent-tests :fixtures (db-path-fixture mongrel2-agent-fixture kill-everything-fixture))
+    (:seq (:eql :server-need-filled)
+          (:eql :handler-need-filled)
+          (:eql :handler-announced))
+  (list
+   (with-agent-conversation (m e) mongrel2-uuid
+     (zmq:send! e (prepare-message
+                   `(:agent :need
+                            :need  :server
+                            :server (:name "forwarder" :port 6969 :hosts ("api.example.com")))))
+     (do* ((msg (parse-message (read-message m))
+                (parse-message (read-message m)))
+           (filled (and (equalp (car msg) :filled) msg)
+                   (or filled
+                       (and (equalp (car msg) :filled) msg))))
+          ((and filled
+                (getf filled :server))
+           (log-for (trace mongrel2-agent::agent-needs) "Filled: ~A" msg)
+           :server-need-filled)))
+
+   (with-agent-conversation (m e) mongrel2-uuid
+     (zmq:send! e (prepare-message
+                   `(:agent :need
+                            :need  :handler
+                            :handler (:server "forwarder" :hosts ("api.example.com") :route "/" :name "api"))))
+     (do* ((msg (parse-message (read-message m))
+                (parse-message (read-message m)))
+           (filled (and (equalp (car msg) :filled) msg)
+                   (or filled
+                       (and (equalp (car msg) :filled) msg))))
+          ((and filled
+                (getf filled :handler))
+           (log-for (trace mongrel2-agent::agent-needs) "Filled: ~A" msg)
+           :handler-need-filled)))
+
+   (with-agent-conversation (m e) mongrel2-uuid
+     (do* ((msg (parse-message (read-message m :timeout 1))
+                (parse-message (read-message m :timeout 1)))
+           (infop (equalp (car msg) :agent) (equalp (car msg) :agent))
+           (info (getf msg :info) (getf msg :info))
+           (provides (and infop (getf info :provides))
+                     (and infop (getf info :provides)))
+           (servers (getf provides :servers)
+                    (getf provides :servers)))
+          ((and provides servers)
+           (let* ((server (assoc "forwarder" servers :test #'string=))
+                  (server-info (cdr server))
+
+                  (api (assoc "api" server-info :test #'string=))
+                  (api-info (cdr api)))
+             (and api-info
+                  (getf api-info :send)
+                  (getf api-info :recv)
+                  :handler-announced)))))))

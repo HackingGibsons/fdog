03e8933afab7c01603fb7fe847228d17b125b6df
Yaroslav Shirokov <slava@hackinggibsons.com>
2012-01-11 14:27:59 -0500

    [afdog-request-agent] Set up and tear down of the requesticle socket.

--- src/fdog/agents/request-processing/requesticle-organ.lisp
+++ src/fdog/agents/request-processing/requesticle-organ.lisp
@@ -13,3 +13,19 @@ at the request sock.")
 (defmethod initialize-instance :after ((organ agent-requesticle) &key)
   "Attach the requesticle behaviors."
   (make-have-hearing organ))
+
+(defmethod agent-boot :after ((agent standard-agent) (requesticle agent-requesticle) &rest options)
+  "Init the requesticle sock"
+  (declare (ignorable options))
+  (log-for (trace requesticle) "Requesticle constructing socket.")
+
+  (setf (request-sock requesticle)
+        (zmq:socket (agent-context agent) zmq:pull)))
+
+(defmethod agent-disconnect :after ((agent standard-agent) (requesticle agent-requesticle) &rest options)
+  "Disconnect the requesticle sock"
+  (declare (ignorable options))
+  (log-for (trace requesticle) "Requesticle destroying socket.")
+
+  (zmq:close (request-sock requesticle))
+  (setf (request-sock requesticle) nil))

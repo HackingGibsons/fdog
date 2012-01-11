5ba24966642879c99dca891f5190ca26387ecebc
Yaroslav Shirokov <slava@hackinggibsons.com>
2012-01-11 17:39:46 -0500

    [afdog-request-agent] We are now connecting to matching handlers.

--- src/fdog/agents/request-processing/behaviors.lisp
+++ src/fdog/agents/request-processing/behaviors.lisp
@@ -1,7 +1,7 @@
 (in-package :request-processing-agent)
 
 (defcategory peer-collection)
-(defmethod heard-message ((agent standard-agent) (organ agent-requesticle) (from (eql :agent)) (type (eql :info)) &rest info)
+(defmethod heard-message ((agent request-processing-agent) (organ agent-requesticle) (from (eql :agent)) (type (eql :info)) &rest info)
   "The requesticle hears an info message. It should examine it to see if contains
 any handlers we're interested in and we should connect to them."
   (let* ((info (getf info :info))
@@ -9,7 +9,12 @@ any handlers we're interested in and we should connect to them."
          (servers (getf provides :servers)))
     (labels ((try-connecting (name &key send recv &allow-other-keys)
                "Connect to the given handler if the name matches."
-               (log-for (trace requesticle peer-collection) "Maybe connect to: ~S (~A ~A)" name send recv))
+               (log-for (trace requesticle peer-collection) "Maybe connect to: ~S (~A ~A)" name send recv)
+               (when (and (string= name (handler-name agent))
+                          (not (gethash send (connected-to organ))))
+                 (log-for (trace requesticle peer-collection) "Connecting to: ~A => ~A[~A]" (getf info :uuid) name send)
+                 (zmq:connect (request-sock organ) send)
+                 (setf (gethash send (connected-to organ)) (getf info :uuid))))
 
              (search-server (server-handlers)
                "Search a server description for handlers"

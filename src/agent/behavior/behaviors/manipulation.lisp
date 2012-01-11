8d1d76bb64c54f62dc698a40be2e749179a74415
Sean Bryant <sean@vitrue.com>
2012-01-11 17:06:33 -0500

    [afdog-hypervisor] make-item needs to respect agent initargs.

--- src/agent/behavior/behaviors/manipulation.lisp
+++ src/agent/behavior/behaviors/manipulation.lisp
@@ -14,29 +14,25 @@
     nil))
 
 (defmethod make-item ((behavior make-things) (what (eql :agent)) info)
-  (format t "Spawn agent: ~A~%" info)
+  (log-for (watch-machine trace) "Spawn agent: ~A" info)
   ;; TODO: Figure out slightly better how to select the spawn class
   (let* ((package (find-package (getf info :package)))
-
-         (class (and package (getf info :class)))
-         (class (and class (find-symbol (symbol-name class) package)))
-         (class (and class (handler-case (find-class class)
-                             (simple-error () nil))))
-
          (uuid (getf info :uuid)))
 
     ;; At this point we have assured that `class' is a real class
     ;; and is a subclass of `standard-agent'
-    (when (and class package)
+    (when package
       ;; Find a spawner in the package of the agent class or use the default
       (let* ((spawner-sym (or (find-symbol (symbol-name '*spawner*) (package-name package))
                               '*spawner*))
              (spawner (symbol-value spawner-sym))
 
-             (initargs `(:uuid ,uuid
-                               :parent-uuid ,(agent-uuid (organ-agent (behavior-organ behavior)))
-                               :parent-mouth ,(mouth-addr (find-organ (organ-agent (behavior-organ behavior)) :mouth))))
-             (runner (apply #'make-runner spawner :class (class-name class) initargs)))
+             (initargs `(,@info 
+                        :uuid ,uuid
+                        :parent-uuid ,(agent-uuid (organ-agent (behavior-organ behavior)))
+                        :parent-mouth ,(mouth-addr (find-organ (organ-agent (behavior-organ behavior)) :mouth))))
+
+             (runner (apply #'make-runner spawner initargs)))
 
         (and runner
              (start runner))

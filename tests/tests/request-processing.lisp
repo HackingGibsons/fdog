7f0ee7fcce3aabfb035c50b94fc66add99f140a9
Yaroslav Shirokov <slava@hackinggibsons.com>
2012-01-11 00:23:41 -0500

    [afdog-request-agent] Testing that I can tell two agents to meet each-other and they will

--- tests/tests/request-processing.lisp
+++ tests/tests/request-processing.lisp
@@ -12,3 +12,20 @@
            :starts)))
 
    (and (running-p request-processing-runner) :running)))
+
+(def-test (request-processing-agent-sees-mongrel2-agent :group request-processing-agent-tests :fixtures (db-path-fixture  mongrel2-agent-fixture request-processing-agent-fixture kill-everything-fixture))
+    (:seq
+     (:eql :informed-peers)
+     (:eql :rp-agent-has-peers))
+  (list
+   (and (tell-agent-about request-processing-uuid mongrel2-uuid)
+        :informed-peers)
+
+   (with-agent-conversation (m e :timeout 35) request-processing-uuid
+     (do* ((msg (parse-message (read-message m))
+                (parse-message (read-message m)))
+           (info (getf msg :info) (getf msg :info))
+           (peers (getf info :peers) (getf info :peers)))
+          (peers
+           :rp-agent-has-peers)))))
+

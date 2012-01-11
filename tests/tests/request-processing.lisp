3f54181a26df62173ce10c2163df98bdbf19cd36
Yaroslav Shirokov <slava@hackinggibsons.com>
2012-01-11 16:52:44 -0500

    [afdog-request-agent] Test that the handler will connect to a peer it should discover.

--- tests/tests/request-processing.lisp
+++ tests/tests/request-processing.lisp
@@ -59,3 +59,54 @@
           (provides (getf info :provides)
                     (getf info :provides)))
          (provides (getf provides :request-processing)))))
+
+(def-test (request-processing-agent-connects-to-matching-handler
+           :group request-processing-agent-tests
+           :fixtures (db-path-fixture mongrel2-agent-fixture request-processing-agent-fixture kill-everything-fixture))
+    (:seq (:eql :informed-peers)
+          (:eql :server-need-filled)
+          (:eql :handler-need-filled)
+          (:eql :connected-to-one))
+  (list
+   (and (tell-agent-about request-processing-uuid mongrel2-uuid)
+        :informed-peers)
+
+   (with-agent-conversation (m e) mongrel2-uuid
+     (zmq:send! e (prepare-message
+                   `(:agent :need
+                            :need  :server
+                            :server (:name "forwarder" :port 6969 :hosts ("api.example.com")))))
+     (do* ((msg (parse-message (read-message m))
+                (parse-message (read-message m)))
+           (filled (and (equalp (car msg) :filled) msg)
+                   (or filled
+                       (and (equalp (car msg) :filled) msg))))
+          ((and filled
+                (getf filled :server))
+           :server-need-filled)))
+
+
+   (with-agent-conversation (m e) mongrel2-uuid
+     (zmq:send! e (prepare-message
+                   `(:agent :need
+                            :need  :handler
+                            :handler (:server "forwarder" :hosts ("api.example.com") :route "/" :name "api"))))
+     (do* ((msg (parse-message (read-message m))
+                (parse-message (read-message m)))
+           (filled (and (equalp (car msg) :filled) msg)
+                   (or filled
+                       (and (equalp (car msg) :filled) msg))))
+          ((and filled
+                (getf filled :handler))
+           (log-for (trace mongrel2-agent::agent-needs) "Filled: ~A" msg)
+           :handler-need-filled)))
+
+   (with-agent-conversation (m e) request-processing-uuid
+    (do* ((msg (parse-message (read-message m))
+               (parse-message (read-message m)))
+          (info (getf msg :info)
+                (getf msg :info))
+          (requesticle (getf info :requesticle)
+                       (getf info :requesticle)))
+         ((equalp (getf requesticle :peers) 1)
+          :connected-to-one)))))

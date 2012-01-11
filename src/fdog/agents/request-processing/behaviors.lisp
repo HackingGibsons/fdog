321130e822280c9ad9df8a9d90a969baf56e3f2c
Yaroslav Shirokov <slava@hackinggibsons.com>
2012-01-11 16:51:44 -0500

    [afdog-request-agent] Adding a category to log peer collection of the requesticle

--- src/fdog/agents/request-processing/behaviors.lisp
+++ src/fdog/agents/request-processing/behaviors.lisp
@@ -1,6 +1,7 @@
 (in-package :request-processing-agent)
 
+(defcategory peer-collection)
 (defmethod heard-message ((agent standard-agent) (organ agent-requesticle) (from (eql :agent)) (type (eql :info)) &rest info)
   "The requesticle hears an info message. It should examine it to see if contains
 any handlers we're interested in and we should connect to them."
-  (log-for (trace requesticle) "Requesticle hears info: ~S" info))
+  (log-for (trace requesticle peer-collection) "Requesticle hears info: ~S" info))

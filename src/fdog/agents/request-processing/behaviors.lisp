a834ce50ea168c218076429b233ce5b41c1dee01
Yaroslav Shirokov <slava@hackinggibsons.com>
2012-01-11 17:33:39 -0500

    [afdog-request-agent] Correctly destructure the endpoint data as it comes in

--- src/fdog/agents/request-processing/behaviors.lisp
+++ src/fdog/agents/request-processing/behaviors.lisp
@@ -4,4 +4,21 @@
 (defmethod heard-message ((agent standard-agent) (organ agent-requesticle) (from (eql :agent)) (type (eql :info)) &rest info)
   "The requesticle hears an info message. It should examine it to see if contains
 any handlers we're interested in and we should connect to them."
-  (log-for (trace requesticle peer-collection) "Requesticle hears info: ~S" info))
+  (let* ((info (getf info :info))
+         (provides (getf info :provides))
+         (servers (getf provides :servers)))
+    (labels ((try-connecting (name &key send recv &allow-other-keys)
+               "Connect to the given handler if the name matches."
+               (log-for (trace requesticle peer-collection) "Maybe connect to: ~S (~A ~A)" name send recv))
+
+             (search-server (server-handlers)
+               "Search a server description for handlers"
+               (log-for (trace requesticle peer-collection) "Destructuring: ~S" server-handlers)
+               (destructuring-bind (server &rest handlers) server-handlers
+                 (declare (ignorable server))
+                 (log-for (trace requesticle peer-collection) "Handlers: ~S" handlers)
+                 (mapc #'(lambda (handler) (apply #'try-connecting handler)) handlers))))
+
+      (log-for (trace requesticle peer-collection) "Requesticle hears info: ~S" info)
+      (log-for (trace requesticle peer-collection) "Found servers: ~S" servers)
+      (mapc #'search-server servers))))

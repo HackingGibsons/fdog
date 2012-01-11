a7ccb58ef8d4cd8f187be7be2000ae7db2a04da3
Sean Bryant <sean@vitrue.com>
2012-01-10 20:31:38 -0500

    [afdog-hypervisor] Export the afdog-hypervisor-agent

--- src/fdog/package.lisp
+++ src/fdog/package.lisp
@@ -1,5 +1,7 @@
 (defpackage #:fdog
   (:use :mongrel2-agent)
-  (:export :mongrel2-agent))
+  (:use :afdog-hypervisor-agent)
+  (:export :mongrel2-agent
+           :afdog-hypervisor-agent))
 
 (in-package :fdog)

fba4ad58741dba83e030c5ca408d7906721fba59
Sean Bryant <sean@vitrue.com>
2012-01-11 19:18:37 -0500

    [afdog-hypervisor] Test against the correct agent symbol.

--- tests/tests/afdog-hypervisor.lisp
+++ tests/tests/afdog-hypervisor.lisp
@@ -1,17 +1,17 @@
 (in-package :afdog-tests)
 
-(def-test (afh-can-spawn-mongrel2-agent :group afdog-hypervisor-agent-tests :fixtures (afdog-hypervisor-agent-fixture))
+(def-test (afh-can-spawn-mongrel2-agent :group afdog-hypervisor-agent-tests :fixtures (db-path-fixture afdog-hypervisor-agent-fixture))
     (:eql :mongrel2-running)
-  (with-agent-conversation (m e  :timeout 60) afdog-hypervisor-uuid
+  (with-agent-conversation (m e :timeout 60) afdog-hypervisor-uuid
     (do* ((connected (awhen (make-hash-table :test 'equalp)
                        (setf (gethash afdog-hypervisor-uuid it) :connected) 
-                       it) 
+                       it)
                      connected)
           (msg (parse-message (read-message m)) 
               (parse-message  (read-message m)))
           (info (getf msg :info) (getf msg :info))
           (peers (getf info :peers) (getf info :peers)))          
-         ((string-equal "mongrel2-agent" (symbol-name (getf info :type)))
+         ((string-equal "mongrel2-test-agent" (symbol-name (getf info :type)))
           :mongrel2-running)
       (mapc #'(lambda (peer)
                 (destructuring-bind (uuid &rest comm-info) peer

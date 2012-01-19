9335aca55d0e58e72f920cd1071786c2c0c1d13a
Reed Lipman <rmlipman@gmail.com>
2012-01-19 13:31:59 -0500

    announce forwarders as part of agent announce

--- src/fdog/agents/forwarder/forwarder-agent.lisp
+++ src/fdog/agents/forwarder/forwarder-agent.lisp
@@ -28,8 +28,7 @@
 
 ;; Hooks
 (defmethod agent-provides :around ((agent forwarder-agent))
-  ;; TODO announce forwarders - is it okay to store them in a slot?
-  (call-next-method))
+  (append (call-next-method) `(:forwarders ,(forwarders agent))))
 
 (defmethod agent-special-event :after ((agent forwarder-agent) (event-head (eql :boot)) event)
   "Boot event for a child agent."

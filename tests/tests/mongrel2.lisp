a020089861b0f20b28f0deafc5d499775352c926
Yaroslav Shirokov <slava@hackinggibsons.com>
2012-01-19 18:03:43 -0500

    [afdog-supervised-mongrel2] Adding back: mongrel2-agent-announces-handler

--- tests/tests/mongrel2.lisp
+++ tests/tests/mongrel2.lisp
@@ -646,9 +646,6 @@
                   (find "api.example.com" hosts :test #'equalp :key #'fdog-models:mongrel2-host-name)))
            :hosts-gone)))))
 
-;; HERE BE DRAGONS
-;; TODO: The bellow tests are being ported to a supervised and persistent
-;;       mongrel2-agent They are being deleted as they are ported.
 (def-test (mongrel2-agent-can-make-handler :group mongrel2-agent-tests)
     (:seq (:eql :server-need-filled)
           (:eql :server-found)
@@ -755,64 +752,67 @@
           (prog1 :handler-has-endpoint
             (log-for (trace mongrel2-agent::agent-needs) "Target: ~A" target))))))
 
-;; (def-test (mongrel2-agent-announces-handler :group mongrel2-agent-tests :fixtures (db-path-fixture mongrel2-agent-fixture kill-everything-fixture))
-;;     (:seq (:eql :server-need-filled)
-;;           (:eql :handler-need-filled)
-;;           (:eql :handler-announced))
-;;   (list
-;;    (with-agent-conversation (m e) mongrel2-uuid
-;;      (zmq:send! e (prepare-message
-;;                    `(:agent :need
-;;                             :need  :server
-;;                             :server (:name "forwarder" :port 6969 :hosts ("api.example.com")))))
-;;      (do* ((msg (parse-message (read-message m))
-;;                 (parse-message (read-message m)))
-;;            (filled (and (equalp (car msg) :filled) msg)
-;;                    (or filled
-;;                        (and (equalp (car msg) :filled) msg))))
-;;           ((and filled
-;;                 (getf filled :server))
-;;            (log-for (trace mongrel2-agent::agent-needs) "Filled: ~A" msg)
-;;            :server-need-filled)))
+;; HERE BE DRAGONS
+;; TODO: The bellow tests are being ported to a supervised and persistent
+;;       mongrel2-agent They are being deleted as they are ported.
+(def-test (mongrel2-agent-announces-handler :group mongrel2-agent-tests)
+    (:seq (:eql :server-need-filled)
+          (:eql :handler-need-filled)
+          (:eql :handler-announced))
+  (list
+   (with-agent-conversation (m e) mongrel2-uuid
+     (zmq:send! e (prepare-message
+                   `(:agent :need
+                            :need  :server
+                            :server (:name "forwarder" :port 6969 :hosts ("api.example.com")))))
+     (do* ((msg (parse-message (read-message m))
+                (parse-message (read-message m)))
+           (filled (and (equalp (car msg) :filled) msg)
+                   (or filled
+                       (and (equalp (car msg) :filled) msg))))
+          ((and filled
+                (getf filled :server))
+           (log-for (trace mongrel2-agent::agent-needs) "Filled: ~A" msg)
+           :server-need-filled)))
 
-;;    (with-agent-conversation (m e) mongrel2-uuid
-;;      (zmq:send! e (prepare-message
-;;                    `(:agent :need
-;;                             :need  :handler
-;;                             :handler (:server "forwarder" :hosts ("api.example.com") :route "/admin/" :name "api-admin"))))
-;;      (zmq:send! e (prepare-message
-;;                    `(:agent :need
-;;                             :need  :handler
-;;                             :handler (:server "forwarder" :hosts ("api.example.com") :route "/" :name "api"))))
-;;      (do* ((msg (parse-message (read-message m))
-;;                 (parse-message (read-message m)))
-;;            (filled (and (equalp (car msg) :filled) msg)
-;;                    (or filled
-;;                        (and (equalp (car msg) :filled) msg))))
-;;           ((and filled
-;;                 (getf filled :handler))
-;;            (log-for (trace mongrel2-agent::agent-needs) "Filled: ~A" msg)
-;;            :handler-need-filled)))
+   (with-agent-conversation (m e) mongrel2-uuid
+     (zmq:send! e (prepare-message
+                   `(:agent :need
+                            :need  :handler
+                            :handler (:server "forwarder" :hosts ("api.example.com") :route "/admin/" :name "api-admin"))))
+     (zmq:send! e (prepare-message
+                   `(:agent :need
+                            :need  :handler
+                            :handler (:server "forwarder" :hosts ("api.example.com") :route "/" :name "api"))))
+     (do* ((msg (parse-message (read-message m))
+                (parse-message (read-message m)))
+           (filled (and (equalp (car msg) :filled) msg)
+                   (or filled
+                       (and (equalp (car msg) :filled) msg))))
+          ((and filled
+                (getf filled :handler))
+           (log-for (trace mongrel2-agent::agent-needs) "Filled: ~A" msg)
+           :handler-need-filled)))
 
-;;    (with-agent-conversation (m e) mongrel2-uuid
-;;      (do* ((msg (parse-message (read-message m :timeout 1))
-;;                 (parse-message (read-message m :timeout 1)))
-;;            (infop (equalp (car msg) :agent) (equalp (car msg) :agent))
-;;            (info (getf msg :info) (getf msg :info))
-;;            (provides (and infop (getf info :provides))
-;;                      (and infop (getf info :provides)))
-;;            (servers (getf provides :servers)
-;;                     (getf provides :servers)))
-;;           ((and provides servers)
-;;            (let* ((server (assoc "forwarder" servers :test #'string=))
-;;                   (server-info (cdr server))
+   (with-agent-conversation (m e) mongrel2-uuid
+     (do* ((msg (parse-message (read-message m :timeout 1))
+                (parse-message (read-message m :timeout 1)))
+           (infop (equalp (car msg) :agent) (equalp (car msg) :agent))
+           (info (getf msg :info) (getf msg :info))
+           (provides (and infop (getf info :provides))
+                     (and infop (getf info :provides)))
+           (servers (getf provides :servers)
+                    (getf provides :servers)))
+          ((and provides servers)
+           (let* ((server (assoc "forwarder" servers :test #'string=))
+                  (server-info (cdr server))
 
-;;                   (api (assoc "api" server-info :test #'string=))
-;;                   (api-info (cdr api)))
-;;              (and api-info
-;;                   (getf api-info :send)
-;;                   (getf api-info :recv)
-;;                   :handler-announced)))))))
+                  (api (assoc "api" server-info :test #'string=))
+                  (api-info (cdr api)))
+             (and api-info
+                  (getf api-info :send)
+                  (getf api-info :recv)
+                  :handler-announced)))))))
 
 ;; (def-test (mongrel2-agent-updates-handler :group mongrel2-agent-tests :fixtures (db-path-fixture mongrel2-agent-fixture kill-everything-fixture))
 ;;     (:seq (:eql :handler-filled)

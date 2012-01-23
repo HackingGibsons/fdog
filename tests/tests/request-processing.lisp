b9c7f0c610dbbebe5723b42b40c56755b8912a5c
Yaroslav Shirokov <slava@hackinggibsons.com>
2012-01-23 18:21:53 -0500

    [afdog-request-agent-tests-hypervised] Base request processing agent tests are now hypervised-mongrel2 safe.

--- tests/tests/request-processing.lisp
+++ tests/tests/request-processing.lisp
@@ -13,88 +13,78 @@
 
    (and (running-p request-processing-runner) :running)))
 
-;; (def-test (request-processing-agent-sees-mongrel2-agent :group request-processing-agent-tests :fixtures (db-path-fixture  mongrel2-agent-fixture request-processing-agent-fixture kill-everything-fixture))
-;;     (:seq
-;;      (:eql :informed-peers)
-;;      (:eql :rp-agent-has-peers)
-;;      (:eql :m2-agent-has-peers))
-;;   (list
-;;    (and (tell-agent-about request-processing-uuid mongrel2-uuid)
-;;         :informed-peers)
+(def-test (request-processing-agent-sees-mongrel2-agent :group request-processing-agent-tests)
+    (:seq
+     (:eql :rp-agent-has-peers)
+     (:eql :m2-agent-has-peers))
+  (list
+   (with-agent-conversation (m e :timeout 35) request-processing-uuid
+     (do* ((msg (parse-message (read-message m))
+                (parse-message (read-message m)))
+           (info (getf msg :info) (getf msg :info))
+           (peers (getf info :peers) (getf info :peers)))
+          (peers
+           :rp-agent-has-peers)))
 
-;;    (with-agent-conversation (m e :timeout 35) request-processing-uuid
-;;      (do* ((msg (parse-message (read-message m))
-;;                 (parse-message (read-message m)))
-;;            (info (getf msg :info) (getf msg :info))
-;;            (peers (getf info :peers) (getf info :peers)))
-;;           (peers
-;;            :rp-agent-has-peers)))
+   (with-agent-conversation (m e :timeout 35) mongrel2-uuid
+     (do* ((msg (parse-message (read-message m))
+                (parse-message (read-message m)))
+           (info (getf msg :info) (getf msg :info))
+           (peers (getf info :peers) (getf info :peers)))
+          (peers
+           :m2-agent-has-peers)))))
 
-;;    (with-agent-conversation (m e :timeout 35) mongrel2-uuid
-;;      (do* ((msg (parse-message (read-message m))
-;;                 (parse-message (read-message m)))
-;;            (info (getf msg :info) (getf msg :info))
-;;            (peers (getf info :peers) (getf info :peers)))
-;;           (peers
-;;            :m2-agent-has-peers)))))
+(def-test (request-processing-agent-announces-connected-count :group request-processing-agent-tests)
+    (:predicate numberp)
+  (with-agent-conversation (m e) request-processing-uuid
+    (do* ((msg (parse-message (read-message m))
+               (parse-message (read-message m)))
+          (info (getf msg :info)
+                (getf msg :info))
+          (requesticle (getf info :requesticle)
+                       (getf info :requesticle)))
+         (requesticle (getf requesticle :peers)))))
 
-;; (def-test (request-processing-agent-announces-connected-count :group request-processing-agent-tests :fixtures (request-processing-agent-fixture kill-everything-fixture))
-;;     (:eql 0)
-;;   (with-agent-conversation (m e) request-processing-uuid
-;;     (do* ((msg (parse-message (read-message m))
-;;                (parse-message (read-message m)))
-;;           (info (getf msg :info)
-;;                 (getf msg :info))
-;;           (requesticle (getf info :requesticle)
-;;                        (getf info :requesticle)))
-;;          (requesticle (getf requesticle :peers)))))
+(def-test (request-processing-agent-announces-provides :group request-processing-agent-tests)
+    (:equalp "api")
+  (with-agent-conversation (m e) request-processing-uuid
+    (do* ((msg (parse-message (read-message m))
+               (parse-message (read-message m)))
+          (info (getf msg :info)
+                (getf msg :info))
+          (provides (getf info :provides)
+                    (getf info :provides)))
+         (provides (getf provides :request-processing)))))
 
-;; (def-test (request-processing-agent-announces-provides :group request-processing-agent-tests :fixtures (request-processing-agent-fixture kill-everything-fixture))
-;;     (:equalp "api")
-;;   (with-agent-conversation (m e) request-processing-uuid
-;;     (do* ((msg (parse-message (read-message m))
-;;                (parse-message (read-message m)))
-;;           (info (getf msg :info)
-;;                 (getf msg :info))
-;;           (provides (getf info :provides)
-;;                     (getf info :provides)))
-;;          (provides (getf provides :request-processing)))))
+(def-test (request-processing-agent-connects-to-matching-handler :group request-processing-agent-tests)
+    (:seq (:eql :handler-need-filled)
+          (:eql :connected-to-one))
+  (list
+   (with-agent-conversation (m e) mongrel2-uuid
+     (zmq:send! e (prepare-message
+                   `(:agent :need
+                            :need  :handler
+                            :handler (:server "control" :hosts ("api.example.com") :route "/" :name "api"))))
+     (zmq:send! e (prepare-message
+                   `(:agent :need
+                            :need  :handler
+                            :handler (:server "control" :hosts ("api.example.com") :route "/ping/" :name "ping"))))
+     (do* ((msg (parse-message (read-message m))
+                (parse-message (read-message m)))
+           (filled (and (equalp (car msg) :filled) msg)
+                   (or filled
+                       (and (equalp (car msg) :filled) msg))))
+          ((and filled
+                (getf filled :handler))
+           (log-for (trace mongrel2-agent::agent-needs) "Filled: ~A" msg)
+           :handler-need-filled)))
 
-;; (def-test (request-processing-agent-connects-to-matching-handler
-;;            :group request-processing-agent-tests
-;;            :fixtures (db-path-fixture mongrel2-agent-fixture request-processing-agent-fixture kill-everything-fixture))
-;;     (:seq (:eql :informed-peers)
-;;           (:eql :handler-need-filled)
-;;           (:eql :connected-to-one))
-;;   (list
-;;    (and (tell-agent-about request-processing-uuid mongrel2-uuid)
-;;         :informed-peers)
-
-;;    (with-agent-conversation (m e) mongrel2-uuid
-;;      (zmq:send! e (prepare-message
-;;                    `(:agent :need
-;;                             :need  :handler
-;;                             :handler (:server "control" :hosts ("api.example.com") :route "/" :name "api"))))
-;;      (zmq:send! e (prepare-message
-;;                    `(:agent :need
-;;                             :need  :handler
-;;                             :handler (:server "control" :hosts ("api.example.com") :route "/ping/" :name "ping"))))
-;;      (do* ((msg (parse-message (read-message m))
-;;                 (parse-message (read-message m)))
-;;            (filled (and (equalp (car msg) :filled) msg)
-;;                    (or filled
-;;                        (and (equalp (car msg) :filled) msg))))
-;;           ((and filled
-;;                 (getf filled :handler))
-;;            (log-for (trace mongrel2-agent::agent-needs) "Filled: ~A" msg)
-;;            :handler-need-filled)))
-
-;;    (with-agent-conversation (m e) request-processing-uuid
-;;      (do* ((msg (parse-message (read-message m))
-;;                 (parse-message (read-message m)))
-;;            (info (getf msg :info)
-;;                  (getf msg :info))
-;;            (requesticle (getf info :requesticle)
-;;                         (getf info :requesticle)))
-;;           ((equalp (getf requesticle :peers) 1)
-;;            :connected-to-one)))))
+   (with-agent-conversation (m e) request-processing-uuid
+     (do* ((msg (parse-message (read-message m))
+                (parse-message (read-message m)))
+           (info (getf msg :info)
+                 (getf msg :info))
+           (requesticle (getf info :requesticle)
+                        (getf info :requesticle)))
+          ((equalp (getf requesticle :peers) 1)
+           :connected-to-one)))))

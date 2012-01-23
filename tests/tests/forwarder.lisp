2aba78fee8726a96c46e8eef138ed4c4c68a295b
Reed Lipman <rmlipman@gmail.com>
2012-01-23 11:22:14 -0500

    dump existing test work

--- tests/tests/forwarder.lisp
+++ tests/tests/forwarder.lisp
@@ -2,17 +2,23 @@
 
 ;;; Fixtures needed: mongrel2-agent, forwarder-agent
 
-(def-test (forwarder-agent-create :group forwarder-agent-tests) (:eql :pending)
+(def-test (can-create-forwarder :group forwarder-agent-tests)
+    (:seq (:eql :need-filled)
+          (:eql :saw-forwarder)
+          (:eql :server-exists)
+          (:eql :handler-exists))
   ;;; Announce "need forwarder named foo"
   ;;; the handler gets created and announced
   ;;; check the handler exists
   (list
-   (with-agent-conversation (m e) agent-uuid
+   (with-agent-conversation (m e) forwarder-agent-uuid
+     (log-for (trace) "before need")
      (zmq:send! e (prepare-message
                    `(:agent :need
                             :need :forwarder
-                            :forwarder ;; TODO
+                            :forwarder (:name "test" :hostpaths (("api.example.com" . "/")))
                             )))
+     (log-for (trace) "after need")
      (do* ((msg (parse-message (read-message m))
                 (parse-message (read-message m)))
            (filled (and (equalp (car msg) :filled) msg)
@@ -20,14 +26,18 @@
                        (and (equalp (car msg) :filled) msg))))
           ((and filled
                 (getf filled :forwarder))
-           (log-for (trace fdog-agent::agent-needs) "Filled: ~A" msg))))
-   )
-  :pending)
+           :need-filled)))
+   (with-agent-conversation (m e) forwarder-agent-uuid
+     (do* ((msg (parse-message (read-message m))
+                (parse-message (read-message m)))
+           (forwarders (getf (getf (getf msg :info) :provides) :forwarders)))
+          ((and forwarders
+                (find "test" (loop for i in hostpaths collect (car i)) :test #'string=))
+           :saw-forwarder)))
+   :server-exists
+   :handler-exists))
 
-(def-test (forwarder-agent-update :group forwarder-agent-tests) (:eql :pending)
-  ;;; Announce "need forwarder foo, new config: (different-config)"
-  ;;; Handler gets updated, announced when done
-  ;;; check the configs differ
+(def-test (can-create-forwarder-with-multiple-hostpaths :group forwarder-agent-tests) (:eql :pending)
   :pending)
 
 (def-test (forwarder-agent-remove :group forwarder-agent-tests) (:eql :pending)
@@ -35,8 +45,16 @@
   ;;; handler deleted, announced
   :pending)
 
+(def-test (can-remove-multiple-forwarders :group forwarder-agent-tests) (:eql :pending))
+
 (def-test (forwarder-agent-cull :group forwarder-agent-tests) (:eql :pending)
   ;;; Announce "need forwarder culling, save foo and bar"
   ;;; Other handlers deleted
   ;;; check foo and bar exist, but not baz
+  :pending)
+
+(def-test (forwarder-agent-stores-forwarders :group forwarder-agent-tests) (:eql :pending)
+  :pending)
+
+(def-test (forwarder-agent-restores-forwarders-after-restart :group forwarder-agent-tests) (:eql :pending)
   :pending)

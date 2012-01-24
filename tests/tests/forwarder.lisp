fedde71a6cab436a9dfac445b757cb2956ec2301
Reed Lipman <rmlipman@gmail.com>
2012-01-24 15:04:02 -0500

    add forwarder test passes!

--- tests/tests/forwarder.lisp
+++ tests/tests/forwarder.lisp
@@ -1,7 +1,8 @@
 (in-package :afdog-tests)
 
 (def-test (can-create-forwarder :group forwarder-agent-tests)
-    (:seq (:eql :need-filled)
+    (:seq (:eql :peers-exist)
+          (:eql :need-filled)
           (:eql :saw-forwarder)
           (:eql :server-exists)
           (:eql :handler-exists))
@@ -9,6 +10,14 @@
   ;;; the handler gets created and announced
   ;;; check the handler exists
   (list
+   (with-agent-conversation (m e) forwarder-agent-uuid
+     (do* ((msg (parse-message (read-message m))
+                (parse-message (read-message m)))
+           (peers (getf (getf msg :info) :peers)
+                  (getf (getf msg :info) :peers)))
+          (peers
+           :peers-exist)))
+
    (with-agent-conversation (m e) forwarder-agent-uuid
      (log-for (trace) "before need")
      (zmq:send! e (prepare-message
@@ -19,19 +28,40 @@
      (do* ((msg (parse-message (read-message m))
                 (parse-message (read-message m)))
            (filled (and (equalp (car msg) :filled) msg)
-                       (and (equalp (car msg) :filled) msg)))
+                   (and (equalp (car msg) :filled) msg)))
           ((and filled
                 (getf filled :forwarder))
            :need-filled)))
+
    (with-agent-conversation (m e) forwarder-agent-uuid
      (do* ((msg (parse-message (read-message m))
                 (parse-message (read-message m)))
-           (forwarders (getf (getf (getf msg :info) :provides) :forwarders)))
+           (forwarders (getf (getf (getf msg :info) :provides) :forwarders)
+                       (getf (getf (getf msg :info) :provides) :forwarders)))
           ((and forwarders
                 (find "test" (loop for i in forwarders collect (car i)) :test #'string=))
            :saw-forwarder)))
-   :server-exists
-   :handler-exists))
+
+   (with-agent-conversation (m e) mongrel2-uuid
+     (do* ((msg (parse-message (read-message m))
+                (parse-message (read-message m)))
+           (servers (getf (getf (getf msg :info) :provides) :servers)
+                    (getf (getf (getf msg :info) :provides) :servers)))
+          ((and servers
+                (find "forwarder" (loop for i in servers collect (car i)) :test #'string=))
+           :server-exists)))
+
+   (with-agent-conversation (m e) mongrel2-uuid
+     (do* ((msg (parse-message (read-message m))
+                (parse-message (read-message m)))
+           (servers (getf (getf (getf msg :info) :provides) :servers)
+                    (getf (getf (getf msg :info) :provides) :servers))
+           (forwarder-server (assoc "forwarder" servers :test #'string=)
+                             (assoc "forwarder" servers :test #'string=))
+           (handler (assoc "forwarder-test" (cdr forwarder-server) :test #'string=)
+                    (assoc "forwarder-test" (cdr forwarder-server) :test #'string=)))
+          (handler
+               :handler-exists)))))
 
 (def-test (can-create-forwarder-with-multiple-hostpaths :group forwarder-agent-tests) (:eql :pending)
   :pending)

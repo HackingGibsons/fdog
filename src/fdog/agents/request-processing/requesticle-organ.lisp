be1b4f3f45d2e59ad7da71b50394a680f40e1a33
Yaroslav Shirokov <slava@hackinggibsons.com>
2012-02-02 09:23:56 -0500

    [afdog-http] Replacing the bare zmq sockets of the requesticle with an m2cl:handler instance. Defined helpers to keep the API the same as the raw slot one.

--- src/fdog/agents/request-processing/requesticle-organ.lisp
+++ src/fdog/agents/request-processing/requesticle-organ.lisp
@@ -7,13 +7,29 @@
                  :documentation "Stores a mapping of strings representing connected-to peers
 The table stores connections in the form send-addr->host-agent-uuid as strings.
 Just the send address is used as a key but connections are made to both send and recv socks")
-   (request-sock :initform nil
-                 :accessor request-sock)
-   (response-sock :initform nil
-                 :accessor response-sock))
+   (handler :initform (make-instance 'm2cl:handler :pub nil :pull nil)
+            :accessor handler
+            :initarg :handler))
   (:documentation "Responsible for firing events when Mongrel2 requests arrive
 at the request sock.")
   (:default-initargs . (:tag :requesticle)))
+
+(defmethod request-sock ((organ agent-requesticle))
+  "Pass through read access to the request socket"
+  (m2cl:handler-pull-socket (handler organ)))
+(defmethod set-request-sock ((organ agent-requesticle) v)
+  "Pass through write access to the request socket"
+  (setf (m2cl:handler-pull-socket (handler organ)) v))
+(defsetf request-sock set-request-sock)
+
+(defmethod response-sock ((organ agent-requesticle))
+  "Pass through read access to the response socket"
+  (m2cl:handler-pull-socket (handler organ)))
+(defmethod set-response-sock ((organ agent-requesticle) v)
+  "Pass through write access to the response socket"
+  (setf (m2cl:handler-pub-socket (handler organ)) v))
+(defsetf response-sock set-response-sock)
+
 
 (defmethod initialize-instance :after ((organ agent-requesticle) &key)
   "Attach the requesticle behaviors."

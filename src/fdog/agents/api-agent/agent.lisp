b59697d2522dc4f0bf499d53355017ed542048ba
Yaroslav Shirokov <slava@hackinggibsons.com>
2012-02-03 14:57:04 -0500

    [afdog-http] Handle the right handler, and listening to messages from mongrel2 agents that have a control server.

--- src/fdog/agents/api-agent/agent.lisp
+++ src/fdog/agents/api-agent/agent.lisp
@@ -1,7 +1,19 @@
 (in-package :api-agent)
 
 ;; Agent
+(defcategory api-agent)
 (defclass api-agent (request-processing-agent)
   ()
+  (:default-initargs . (:handle "api"))
   (:documentation "This agent establishes a handler for the API endpoint
 and contains the implementation of the afdog API."))
+
+(defmethod heard-message :after ((agent api-agent) (head agent-head) (from (eql :agent)) (info (eql :info)) &rest event)
+  (let* ((info (getf event :info))
+         (provides (getf info :provides))
+         (servers (getf provides :servers))
+         (control (assoc "control" servers :test #'string=)))
+    (multiple-value-bind (name handlers) (values (car control) (cdr control))
+      (let ((handler (cdr (assoc (handler-name agent) handlers :test #'string=))))
+        (when (and name (not handler))
+          (log-for (trace api-agent) "Server: ~S doesn't have handler ~S" name (handler-name agent)))))))

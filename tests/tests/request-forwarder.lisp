4830b4d98e77c064a021146b91e4b77698aae918
Yaroslav Shirokov <slava@hackinggibsons.com>
2012-02-08 19:43:54 -0500

    [afdog-req-forward] Making sure we're asking for the right handler from the agent

--- tests/tests/request-forwarder.lisp
+++ tests/tests/request-forwarder.lisp
@@ -11,3 +11,14 @@
           :starts)))
 
    (and (running-p request-forwarder-runner) :running))
+
+(def-test (request-forwarder-agent-announces-provides :group request-forwarder-agent-tests)
+    (:equalp "forwarder-test-default")
+  (with-agent-conversation (m e) request-forwarder-uuid
+    (do* ((msg (parse-message (read-message m))
+               (parse-message (read-message m)))
+          (info (getf msg :info)
+                (getf msg :info))
+          (provides (getf info :provides)
+                    (getf info :provides)))
+         (provides (getf provides :request-processing)))))

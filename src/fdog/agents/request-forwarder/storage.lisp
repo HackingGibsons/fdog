0c9cf3a5445e833a67b45c0076273b44debcdfdc
Yaroslav Shirokov <slava@hackinggibsons.com>
2012-03-06 16:28:00 -0500

    [afdog-redis-store] The storage and queue hooks fire in the correct order.

--- src/fdog/agents/request-forwarder/storage.lisp
+++ src/fdog/agents/request-forwarder/storage.lisp
@@ -11,17 +11,38 @@
          (second (ppcre:split "--id-([^\s]+)" response :end target-end :with-registers-p t :omit-unmatched-p t)))))
 
 ;; Hooks
-(defmethod request-handler :around ((agent request-forwarder-agent) organ req data)
+(defmethod deliver-request :around (endpoint req)
   "Request storage hook."
-  (declare (ignore data organ))
-  (let ((request (call-next-method)))
-    (log-for (warn request-storage) "TODO: Store the request: [~S]" (babel:octets-to-string (m2cl:request-serialize request)))))
+  (unwind-protect (call-next-method)
+    (store-request (agent endpoint) req)))
 
 (defmethod response-handler :after ((agent request-forwarder-agent) organ data)
   "Response storage hook."
-  (let ((identifier (response-id data)))
-    (log-for (warn request-storage) "TODO: Handle the response [~S] by storing it." (babel:octets-to-string data))))
+  (store-response agent data))
 
 (defmethod delivery-failure-handler ((agent request-forwarder-agent) organ req)
-  (log-for (warn request-storage) "TODO: Queue the request: ~S" (m2cl:request-serialize req)))
+  "Request queue hook."
+  (queue-request agent req))
+
+;; Action methods.
+(defgeneric store-request (agent request)
+  (:documentation "Store the request data.")
+
+  (:method ((agent request-forwarder-agent) req)
+    (log-for (warn request-storage) "TODO: Store the request data of: [~S]." (babel:octets-to-string (m2cl:request-serialize req)))))
+
+(defgeneric store-response (agent response)
+  (:documentation "Store the response data in a way that can be found through
+the request data.")
+
+  (:method ((agent request-forwarder-agent) data)
+    (let ((identifier (response-id data)))
+      (log-for (warn request-storage) "TODO: Handle the response [~S] == [~S] by storing it." identifier (babel:octets-to-string data)))))
+
+(defgeneric queue-request (agent request)
+  (:documentation "Append the request to the queue of requests handled by `agent'")
+
+  (:method ((agent request-forwarder-agent) request)
+    (log-for (warn request-storage) "TODO: Queue the request: [~S]" (babel:octets-to-string (m2cl:request-serialize request)))))
+
 

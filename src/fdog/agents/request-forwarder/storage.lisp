a4ae49406ecce27c1b6616aa5a79de8bac93932a
Yaroslav Shirokov <slava@hackinggibsons.com>
2012-03-06 16:48:05 -0500

    [afdog-redis-store] Linking in the expire request hook

--- src/fdog/agents/request-forwarder/storage.lisp
+++ src/fdog/agents/request-forwarder/storage.lisp
@@ -12,9 +12,14 @@
 
 ;; Hooks
 (defmethod deliver-request :around (endpoint req)
-  "Request storage hook."
-  (unwind-protect (call-next-method)
-    (store-request (agent endpoint) req)))
+  "Request storage and expiry hook."
+  (flet ((do-store (&optional c)
+           (declare (ignore c))
+           (store-request (agent endpoint) req)))
+    (handler-bind ((t #'do-store))
+      (prog1 (call-next-method)
+        (do-store)
+        (expire-request (agent endpoint) req)))))
 
 (defmethod response-handler :after ((agent request-forwarder-agent) organ data)
   "Response storage hook."
@@ -29,7 +34,15 @@
   (:documentation "Store the request data.")
 
   (:method ((agent request-forwarder-agent) req)
-    (log-for (warn request-storage) "TODO: Store the request data of: [~S]." (babel:octets-to-string (m2cl:request-serialize req)))))
+    (let ((id (m2cl:request-header req *request-id-header* (format nil "UNKNOWN-~A" (uuid:make-v4-uuid)))))
+      (log-for (warn request-storage) "TODO: Store the request data of: ~S: [~S]."
+               id (babel:octets-to-string (m2cl:request-serialize req))))))
+
+(defgeneric expire-request (agent request)
+  (:documentation "Set the request to expire after a given time.")
+
+  (:method ((agent request-forwarder-agent) req)
+    (log-for (warn request-storage) "TODO: Expire request: [~S]" (babel:octets-to-string (m2cl:request-serialize req)))))
 
 (defgeneric store-response (agent response)
   (:documentation "Store the response data in a way that can be found through
@@ -44,5 +57,3 @@ the request data.")
 
   (:method ((agent request-forwarder-agent) request)
     (log-for (warn request-storage) "TODO: Queue the request: [~S]" (babel:octets-to-string (m2cl:request-serialize request)))))
-
-

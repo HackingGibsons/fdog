d0bd0bb3b3978da4c48956df61c67bc8d819e938
Reed Lipman <rmlipman@gmail.com>
2012-03-14 15:47:23 -0400

    kill replying to heartbeat

--- src/agent/methods/standard-organ.lisp
+++ src/agent/methods/standard-organ.lisp
@@ -93,7 +93,7 @@ and callbacks for each socket mentioned.")
     (when (and (listp event) (eql (getf event :heart) :beat))
       (setf (last-beat organ) (subseq (append (list (getf event :time)) (last-beat organ)) 0 (keep-beats organ)))
 
-      (log-for (trace) "~A replying to heartbeat." organ)
+      ;; Reply to heartbeat
       (send-message organ :beat `(:uuid ,(organ-uuid organ)
                                   :time ,(get-internal-real-time))))))
 ;; Hard ticks

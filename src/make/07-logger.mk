b5589fc26116f9ffb04bdbce5688784d3122967a
Reed Lipman <rmlipman@gmail.com>
2012-03-15 16:03:53 -0400

    make logger-macports phony

--- src/make/07-logger.mk
+++ src/make/07-logger.mk
@@ -4,8 +4,10 @@ logger: LIBS = -lzmq
 logger: logger.c
 	gcc -o bin/$@ $^ $(OPTFLAGS) $(LIBFLAGS) $(LIBS)
 
+.PHONY: logger-macports
 logger-macports: OPTFLAGS += -I/opt/local/include/
 logger-macports: LIBFLAGS += -L/opt/local/lib/
 logger-macports: logger
 
-
+logger-clean:
+	rm bin/logger

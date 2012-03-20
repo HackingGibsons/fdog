32492d03bb30aa882f0b6f53980f5a63ec482269
Reed Lipman <rmlipman@gmail.com>
2012-03-20 15:17:15 -0400

    request-handler on api-mixin

--- src/fdog/agents/api-agent/request-handler.lisp
+++ src/fdog/agents/api-agent/request-handler.lisp
@@ -1,6 +1,6 @@
 (in-package :api-agent)
 
-(defmethod request-handler ((agent api-agent) organ req raw)
+(defmethod request-handler ((agent api-mixin) organ req raw)
   "Request handler."
   (api-app:api agent organ (handler agent) req raw))
 

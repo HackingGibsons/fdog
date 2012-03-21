2ef280fd9f6711b5641a9de2227374cc30919739
Reed Lipman <rmlipman@gmail.com>
2012-03-21 14:04:06 -0400

    add another test idea

--- tests/tests/accounts.lisp
+++ tests/tests/accounts.lisp
@@ -4,3 +4,6 @@
 ;; No API key - expect 401
 ;; Incorrect API key - expect 401
 ;; Correct API key - expect "correct" behavior (whatever that is)
+
+;; later - test that a correct API key exists in redis and a timeout
+;; exists
